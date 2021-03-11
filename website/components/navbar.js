22cbe3fc5f30529a775b162f6bd677cc70b29637
Alan Shaw <alan.shaw@protocol.ai>
2021-03-11 11:15:18 +0000

    feat: about section words and style tweaks

--- website/components/navbar.js
+++ website/components/navbar.js
@@ -3,15 +3,17 @@ import Button from './button.js'
 
 export default function Navbar () {
   return (
-    <nav className='dn dn-m bg-nsorange flex-ns items-center justify-between ph5 pv3'>
-      <div>
-        <Link href='/#getting-started'><a className='f4 black no-underline underline-hover v-mid'>Getting Started</a></Link>
-        <span className='mh2 v-mid b black'>•</span>
-        <Link href='/#api-docs'><a className='f4 black no-underline underline-hover v-mid'>API Docs</a></Link>
-      </div>
-      <div>
-        <Button wrapperClassName='mr3' href='#'>Register</Button>
-        <Button href='#'>Login</Button>
+    <nav className='dn dn-m db-ns bg-nsorange'>
+      <div className='flex items-center justify-between ph5 pv3 center mw9'>
+        <div>
+          <Link href='/#getting-started'><a className='f4 black no-underline underline-hover v-mid'>Getting Started</a></Link>
+          <span className='mh2 v-mid b black'>•</span>
+          <Link href='/#api-docs'><a className='f4 black no-underline underline-hover v-mid'>API Docs</a></Link>
+        </div>
+        <div>
+          <Button wrapperClassName='mr3' href='#'>Register</Button>
+          <Button href='#'>Login</Button>
+        </div>
       </div>
     </nav>
   )
--- website/pages/index.js
+++ website/pages/index.js
@@ -18,32 +18,56 @@ export default function Home () {
         <meta name='og:title' content='nft.storage' />
         <meta name='twitter:card' content='summary_large_image' />
       </Head>
-
       <Navbar />
       <Hero />
-
       <main>
-        <article className='bg-pink pa5'>
-          <h1 className='chicagoflf'>
-            <HashLink id='about'>About</HashLink>
-          </h1>
-        </article>
-
-        <article className='bg-yellow pa5'>
-          <h1 className='chicagoflf'>
-            <HashLink id='getting-started'>Getting started</HashLink>
-          </h1>
-        </article>
-
-        <article className='bg-blue pa5'>
-          <h1 className='chicagoflf white'>
-            <HashLink id='api-docs'>API Documentation</HashLink>
-          </h1>
-        </article>
+        <About />
+        <GettingStarted />
+        <APIDocs />
       </main>
-
       <Footer />
-
     </div>
+  )
+}
+
+function About () {
+  return (
+    <article className='bg-pink'>
+      <div className='mw9 center pa5'>
+        <h1 className='chicagoflf'>
+          <HashLink id='about'>About</HashLink>
+        </h1>
+        <p className='lh-copy mw-6'>
+          <strong>nft.storage</strong> is a brand new service in private BETA, built specifically for storing <strong>off-chain</strong> NFT data. Data is stored <em>decentralized</em> on <a href='https://ipfs.io' className='black'>IPFS</a> and <em>backed up</em> in cold storage by <a href='https://filecoin.io' className='black'>Filecoin</a>.
+        </p>
+        <div className='tc chicagoflf pv6'>
+          Diagram here
+        </div>
+      </div>
+    </article>
+  )
+}
+
+function GettingStarted () {
+  return (
+    <article className='bg-yellow'>
+      <div className='mw9 center pa5'>
+        <h1 className='chicagoflf tc'>
+          <HashLink id='getting-started'>Getting started</HashLink>
+        </h1>
+      </div>
+    </article>
+  )
+}
+
+function APIDocs () {
+  return (
+    <article className='bg-blue'>
+      <div className='mw9 center pa5'>
+        <h1 className='chicagoflf white'>
+          <HashLink id='api-docs'>API Documentation</HashLink>
+        </h1>
+      </div>
+    </article>
   )
 }
