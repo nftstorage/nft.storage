9be6c182f607f0b3e5f84d06d8d851c069e3b575
Alan Shaw <alan.shaw@protocol.ai>
2021-03-17 10:59:53 +0000

    refactor: wording

--- website/pages/index.js
+++ website/pages/index.js
@@ -14,11 +14,11 @@ export default function Home () {
   return (
     <div className='sans-serif'>
       <Head>
-        <title>nft.storage free storage for NFT data on IPFS, backed up to Filecoin.</title>
+        <title>nft.storage free decentralized storage for NFT data on IPFS and Filecoin.</title>
         <link rel='icon' href='/favicon.ico' />
         <meta
           name='description'
-          content='Unlimited storage of NFT data on IPFS, backed by Filecoin and provided free to NFTHack participants during the hackathon.'
+          content='5GB storage of NFT data on IPFS and Filecoin and provided free to NFTHack participants during the hackathon.'
         />
         <meta property='og:image' content='' />
         <meta name='og:title' content='nft.storage' />
@@ -58,7 +58,7 @@ function About () {
           <HashLink id='about'>About</HashLink>
         </h1>
         <p className='lh-copy mw-none mw-none-m mw6-ns'>
-          <strong>nft.storage</strong> is a brand new service in private BETA, built specifically for storing <strong>off-chain</strong> NFT data. Data is stored <em>decentralized</em> on <a href='https://ipfs.io' className='black'>IPFS</a> and <em>backed up</em> in cold storage by <a href='https://filecoin.io' className='black'>Filecoin</a>.
+          <strong>nft.storage</strong> is a brand new service in private BETA, built specifically for storing <strong>off-chain</strong> NFT data. Data is stored <em>decentralized</em> on <a href='https://ipfs.io' className='black'>IPFS</a> and <a href='https://filecoin.io' className='black'>Filecoin</a>.
         </p>
         <div className='db db-m dn-ns'>
           <div>
