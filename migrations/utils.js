4860cc4d3a04de8f481dd54f724612ba9d86fa8f
Alan Shaw <alan.shaw@protocol.ai>
2021-05-06 09:47:30 +0100

    fix: production table name

--- migrations/utils.js
+++ migrations/utils.js
@@ -1,7 +1,7 @@
 export function findNs(namespaces, env, name) {
-  const prefix = env === 'production' ? '' : `-${env}-`
+  const prefix = env === 'production' ? '' : `${env}-`
   const suffix = env === 'production' ? '' : '_preview'
-  const fqn = `nft-storage${prefix}${name}${suffix}`
+  const fqn = `nft-storage-${prefix}${name}${suffix}`
   const ns = namespaces.find((ns) => ns.title === fqn)
   if (!ns) throw new Error(`KV namespace ${fqn} not found`)
   return ns
