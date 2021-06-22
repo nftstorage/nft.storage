4d54ddcb6a838748dd9fd387e1374b6bbde55274
Alan Shaw <alan.shaw@protocol.ai>
2021-06-22 16:05:17 +0100

    fix: client tests

--- packages/client/test/lib.spec.js
+++ packages/client/test/lib.spec.js
@@ -147,7 +147,7 @@ describe('client', () => {
           uploadedChunks++
         },
       })
-      assert.equal(uploadedChunks, 12)
+      assert.ok(uploadedChunks >= 12)
       assert.equal(cid, expectedCid)
     })
   })
@@ -633,6 +633,12 @@ describe('client', () => {
 
 const MAX_BLOCK_SIZE = 1024 * 1024 * 4
 
+function randomBlockSize() {
+  const max = MAX_BLOCK_SIZE
+  const min = max / 2
+  return Math.random() * (max - min) + min
+}
+
 /**
  * @param {number} targetSize
  * @returns {Promise<AsyncIterable<Uint8Array>>}
@@ -643,7 +649,7 @@ async function randomCar(targetSize) {
   const seen = new Set()
   while (size < targetSize) {
     const bytes = dagCbor.encode(
-      garbage(targetSize / MAX_BLOCK_SIZE, { weights: { CID: 0 } })
+      garbage(randomBlockSize(), { weights: { CID: 0 } })
     )
     const hash = await sha256.digest(bytes)
     const cid = CID.create(1, dagCbor.code, hash)
