f0f955305e9d65b6993f04a18b30673e5f8bc5e6
Alan Shaw <alan.shaw@protocol.ai>
2021-06-22 17:57:24 +0100

    fix: cluster status text for queued pin

--- packages/api/src/cluster.js
+++ packages/api/src/cluster.js
@@ -125,6 +125,6 @@ export function toPSAStatus(status) {
   const pinInfos = Object.values(status.peerMap)
   if (pinInfos.some((i) => i.status === 'pinned')) return 'pinned'
   if (pinInfos.some((i) => i.status === 'pinning')) return 'pinning'
-  if (pinInfos.some((i) => i.status === 'queued')) return 'queued'
+  if (pinInfos.some((i) => i.status === 'pin_queued')) return 'queued'
   return 'failed'
 }
