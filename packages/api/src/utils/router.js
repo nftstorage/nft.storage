ed516fee3c0998b0d4a3e0a94da8307b077682a8
Hugo Dias <hugomrdias@gmail.com>
2021-07-11 21:55:26 +0100

    chore: update more deps

--- packages/api/src/utils/router.js
+++ packages/api/src/utils/router.js
@@ -1,4 +1,4 @@
-import regexparam from 'regexparam'
+import { parse } from 'regexparam'
 import { getSentry } from './debug.js'
 
 /**
@@ -93,7 +93,7 @@ class Router {
       return req.method.toLowerCase() === m
     }
 
-    const parsed = regexparam(route)
+    const parsed = parse(route)
     const routeCondition = (/** @type {Request} */ req) => {
       const url = new URL(req.url)
       const path = url.pathname
