0598f2c77e144e284b352b2b0ff33e0e8b9d00bc
Hugo Dias <hugomrdias@gmail.com>
2021-07-22 12:59:18 +0100

    feat: tokens create and delete

--- packages/api/src/routes/tokens-delete.js
+++ packages/api/src/routes/tokens-delete.js
@@ -1,13 +1,18 @@
 import { validate } from '../utils/auth.js'
 import { JSONResponse } from '../utils/json-response.js'
-import { deleteToken } from '../models/users.js'
 
 /** @type {import('../utils/router.js').Handler} */
 export const tokensDelete = async (event, ctx) => {
-  const { user } = await validate(event, ctx)
+  const { fauna } = await validate(event, ctx)
   const body = await event.request.json()
 
-  await deleteToken(user.issuer, body.name)
+  if (body.id) {
+    await fauna.deleteUserKey({
+      id: body.id,
+    })
+  } else {
+    throw new Error('Token id is required.')
+  }
 
   return new JSONResponse({
     ok: true,
