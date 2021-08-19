06fbc653ad2664e0b241de8ded035670f8140c73
Irakli Gozalishvili <contact@gozala.io>
2021-08-19 14:17:31 -0700

    feat: job keeps track of state

--- packages/database/src/migrations/2021-08-18-token-asset-queue.js
+++ packages/database/src/migrations/2021-08-18-token-asset-queue.js
@@ -11,13 +11,16 @@ const {
   Documents,
   Lambda,
   Paginate,
-  Ref,
+  Call,
+  Update,
+  Now,
   Map,
   Do,
 } = fauna
 
 export const main = async () => await run(await readConfig())
 const MIGRATION_TIME = '2021-08-19T16:23:16.984862Z'
+const KEY = 'status-to-queue-migration-2021-08-18'
 
 /**
  *
@@ -27,24 +30,24 @@ export const run = async (config) => {
   console.log(`🚧 Performing migration`)
 
   const client = new Client({ secret: config.secret })
-  const cursor = config.cursor
-    ? Ref(Collection('TokenAsset'), config.cursor)
-    : undefined
-  const state = { cursor, linked: 0, failed: 0, queued: 0 }
 
-  while (true) {
+  const state = await init(client)
+
+  while (state.data.cursor !== -1) {
     // Fetch page of TokenAsset from the db.
     let { after, data } = /** @type {{after:fauna.Expr|null, data:any[]}} */ (
       await client.query(
         Map(
           Paginate(Documents(Collection('TokenAsset')), {
             size: config.size,
-            after: state.cursor,
+            after: state.data.cursor,
           }),
           Lambda(['ref'], Get(Var('ref')))
         )
       )
     )
+
+    console.log('<<', { after, data: data.length })
 
     // Create a document that corresponds to it's status
     const expressions = []
@@ -56,9 +59,9 @@ export const run = async (config) => {
         break
       }
 
-      switch (tokenAsset.status) {
+      switch (tokenAsset.data.status) {
         case 'Linked':
-          state.linked += 1
+          state.data.metadata.linked += 1
           expressions.push(
             Create(Collection('Analyzed'), {
               data: {
@@ -69,7 +72,7 @@ export const run = async (config) => {
           )
           break
         case 'Queued':
-          state.queued += 1
+          state.data.metadata.queued += 1
           expressions.push(
             Create(Collection('ScheduledAnalyze'), {
               data: {
@@ -79,8 +82,19 @@ export const run = async (config) => {
             })
           )
           break
+        case 'ContentFetchFailed':
+          state.data.metadata.queued += 1
+          expressions.push(
+            Create(Collection('ScheduledAnalyze'), {
+              data: {
+                tokenAsset: tokenAsset.ref,
+                attempt: 2,
+              },
+            })
+          )
+          break
         default:
-          state.failed += 1
+          state.data.metadata.failed += 1
           expressions.push(
             Create(Collection('FailedAnalyze'), {
               data: {
@@ -95,18 +109,72 @@ export const run = async (config) => {
       }
     }
 
+    if (expressions.length > 0) {
+      const { linked, failed, queued } = state.data.metadata
+      state.data.updated = Now()
+      state.data.value = linked + queued + failed
+      state.data.cursor = after || -1
+
+      if (!config.dryrun) {
+        await client.query(
+          Do([
+            ...expressions,
+            Update(state.ref, {
+              data: state.data,
+            }),
+          ])
+        )
+      }
+    }
+
     // If there was nothing we're done
-    if (expressions.length === 0 || after === null) {
-      console.log('🏁 Migration is complete', state)
+    if (expressions.length === 0) {
       break
     } else {
-      if (!config.dryrun) {
-        await client.query(Do(expressions))
-      }
-      state.cursor = after
-      console.log('⏭ Moving to next page', state)
+      console.log('⏭ Page migrated', {
+        ...state.data.metadata,
+        cursor: state.data.cursor,
+      })
     }
   }
+
+  console.log('🏁 Migration is complete', state.data.metadata)
+}
+
+/**
+ * @typedef {{
+ *  ref: fauna.Exp
+ *  data: {
+ *    value: number
+ *    updated: fauna.Expr
+ *    metadata: {
+ *      queued: number
+ *      linked: number
+ *      failed: number
+ *    }
+ *    cursor: fauna.Expr | undefined | -1
+ *  }
+ * }} State
+ *
+ * @param {fauna.Client} client
+ * @returns {Promise<State>}
+ */
+const init = async (client) => {
+  const state =
+    (await client.query(Call('findMetricByKey', KEY))) ||
+    (await client.query(
+      Create(Collection('Metric'), {
+        data: {
+          key: KEY,
+          value: 0,
+          updated: Now(),
+          metadata: { queued: 0, linked: 0, failed: 0 },
+          cursor: undefined,
+        },
+      })
+    ))
+
+  return /** @type{State} */ (state)
 }
 
 script({ ...import.meta, main })
