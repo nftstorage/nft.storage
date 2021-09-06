6157fbb2d3c694a92018cf0256c97d62bdf193d3
Alan Shaw <alan.shaw@protocol.ai>
2021-09-06 18:17:18 +0100

    fix: consistent casing

--- packages/db/tables.sql
+++ packages/db/tables.sql
@@ -29,8 +29,8 @@ CREATE TABLE IF NOT EXISTS nft_storage.user (
   github TEXT,
   email TEXT NOT NULL,
   public_address TEXT NOT NULL UNIQUE,
-  inserted_at timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL,
-  updated_at timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL
+  inserted_at TIMESTAMP WITH TIME ZONE DEFAULT timezone('utc'::text, now()) NOT NULL,
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT timezone('utc'::text, now()) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS nft_storage.auth_key (
@@ -38,16 +38,16 @@ CREATE TABLE IF NOT EXISTS nft_storage.auth_key (
   name TEXT NOT NULL,
   secret TEXT NOT NULL,
   user_id TEXT NOT NULL REFERENCES user ( id ),
-  inserted_at timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL,
-  updated_at timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL
+  inserted_at TIMESTAMP WITH TIME ZONE DEFAULT timezone('utc'::text, now()) NOT NULL,
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT timezone('utc'::text, now()) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS nft_storage.content (
   -- normalized base32 v1
   cid TEXT PRIMARY KEY,
   dag_size BIGINT,
-  inserted_at timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL,
-  updated_at timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL
+  inserted_at TIMESTAMP WITH TIME ZONE DEFAULT timezone('utc'::text, now()) NOT NULL,
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT timezone('utc'::text, now()) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS nft_storage.pin (
@@ -55,8 +55,8 @@ CREATE TABLE IF NOT EXISTS nft_storage.pin (
   status pin_status NOT NULL,
   content_cid TEXT NOT NULL REFERENCES content ( cid ),
   service nft_storage.service NOT NULL,
-  inserted_at timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL,
-  updated_at timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL,
+  inserted_at TIMESTAMP WITH TIME ZONE DEFAULT timezone('utc'::text, now()) NOT NULL,
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT timezone('utc'::text, now()) NOT NULL,
   UNIQUE (content_cid, service)
 );
 
@@ -64,8 +64,8 @@ CREATE TABLE IF NOT EXISTS nft_storage.upload (
   id BIGSERIAL PRIMARY KEY,
   user_id TEXT NOT NULL REFERENCES user ( id ),
   auth_key_id BIGINT REFERENCES auth_key ( id ),
-  source_cid text NOT NULL,
-  content_cid text NOT NULL REFERENCES content ( cid ),
+  source_cid TEXT NOT NULL,
+  content_cid TEXT NOT NULL REFERENCES content ( cid ),
   name TEXT,
   type nft_storage.upload_type NOT NULL,
   -- MIME type of the upload data as sent in the request.
@@ -73,7 +73,7 @@ CREATE TABLE IF NOT EXISTS nft_storage.upload (
   files jsonb,
   origins jsonb,
   meta jsonb,
-  inserted_at timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL,
-  updated_at timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL,
-  deleted_at timestamp with time zone DEFAULT timezone('utc'::text, now())
+  inserted_at TIMESTAMP WITH TIME ZONE DEFAULT timezone('utc'::text, now()) NOT NULL,
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT timezone('utc'::text, now()) NOT NULL,
+  deleted_at TIMESTAMP WITH TIME ZONE DEFAULT timezone('utc'::text, now())
 );
