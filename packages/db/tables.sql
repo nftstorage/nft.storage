d4777e2a146c99a58a9fde3ad526afaf8a48b59d
Alan Shaw <alan.shaw@protocol.ai>
2021-09-06 18:07:06 +0100

    refactor: schema name

--- packages/db/tables.sql
+++ packages/db/tables.sql
@@ -1,24 +1,24 @@
-CREATE SCHEMA nftstorage;
+CREATE SCHEMA nft_storage;
 
-CREATE TYPE nftstorage.pin_status AS ENUM (
+CREATE TYPE nft_storage.pin_status AS ENUM (
   'Queued',
   'Pinning',
   'Pinned',
   'Failed'
 );
 
-CREATE TYPE nftstorage.service AS ENUM (
+CREATE TYPE nft_storage.service AS ENUM (
   'Pinata',
   'IpfsCluster'
 );
 
-CREATE TYPE nftstorage.upload_type AS ENUM (
+CREATE TYPE nft_storage.upload_type AS ENUM (
   'Car',
   'Blob',
   'Multipart'
 );
 
-CREATE TABLE IF NOT EXISTS nftstorage.user (
+CREATE TABLE IF NOT EXISTS nft_storage.user (
   id BIGSERIAL PRIMARY KEY,
   issuer TEXT,
   sub TEXT,
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS nftstorage.user (
   updated_at timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS nftstorage.auth_key (
+CREATE TABLE IF NOT EXISTS nft_storage.auth_key (
   id BIGSERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   secret TEXT NOT NULL,
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS nftstorage.auth_key (
   updated_at timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS nftstorage.content (
+CREATE TABLE IF NOT EXISTS nft_storage.content (
   -- normalized base32 v1
   cid TEXT PRIMARY KEY,
   dag_size BIGINT,
@@ -48,24 +48,24 @@ CREATE TABLE IF NOT EXISTS nftstorage.content (
   updated_at timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS nftstorage.pin (
+CREATE TABLE IF NOT EXISTS nft_storage.pin (
   id BIGSERIAL PRIMARY KEY,
   status pin_status NOT NULL,
   content_cid TEXT NOT NULL REFERENCES content ( cid ),
-  service nftstorage.service NOT NULL,
+  service nft_storage.service NOT NULL,
   inserted_at timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL,
   updated_at timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL,
   UNIQUE (content_cid, service)
 );
 
-CREATE TABLE IF NOT EXISTS nftstorage.upload (
+CREATE TABLE IF NOT EXISTS nft_storage.upload (
   id BIGSERIAL PRIMARY KEY,
   user_id TEXT NOT NULL REFERENCES user ( id ),
   auth_key_id BIGINT REFERENCES auth_key ( id ),
   source_cid text NOT NULL,
   content_cid text NOT NULL REFERENCES content ( cid ),
   name TEXT,
-  type nftstorage.upload_type NOT NULL,
+  type nft_storage.upload_type NOT NULL,
   files jsonb,
   origins jsonb,
   meta jsonb,
