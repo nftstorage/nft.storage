4a7dd4723dba1ff388918a4e4ff73fd44b38f38c
the-simian <harlinjesse@gmail.com>
2021-09-10 15:30:17 -0500

    more tables

--- packages/database_v2/artifacts/init.sql
+++ packages/database_v2/artifacts/init.sql
@@ -34,6 +34,7 @@ CREATE TYPE pin_service AS ENUM (
 CREATE TABLE IF NOT EXISTS blockchain_block (
     hash TEXT NOT NULL,
     number INTEGER NOT NULL,
+
     updated_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     inserted_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (hash)
@@ -46,8 +47,70 @@ CREATE TABLE IF NOT EXISTS nft (
     mint_time TIMESTAMP(3) NOT NULL,
     token_uri TEXT NOT NULL,
     contract_id TEXT NOT NULL,
-    owner_id TEXT NOT NULL,
+    nft_owner_id TEXT NOT NULL,
+
     updated_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     inserted_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS nfts_by_blockchain_blocks (
+    blockchain_block_hash TEXT NOT NULL,
+    nft_id TEXT NOT NULL,
+
+    updated_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    inserted_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (blockchain_block_hash, nft_id)
+);
+
+CREATE TABLE IF NOT EXISTS nft_owner (
+    id TEXT NOT NULL,
+
+    updated_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    inserted_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS nft_asset (
+    token_uri TEXT NOT NULL,
+    ipfs_URL TEXT,
+    status nft_asset_status NOT NULL,
+    status_text TEXT NOT NULL,
+
+    updated_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    inserted_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (token_uri)
+);
+
+CREATE TABLE IF NOT EXISTS nft_metadata (
+    content_cid TEXT NOT NULL,
+    token_uri TEXT NOT NULL,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    image_uri TEXT NOT NULL,
+
+    updated_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    inserted_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (content_cid)
+);
+
+CREATE TABLE IF NOT EXISTS resource (
+    uri TEXT NOT NULL,
+    status resource_status NOT NULL,
+    status_text TEXT,
+    ipfs_url TEXT,
+    content_cid TEXT,
+
+    updated_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    inserted_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (uri)
+);
+
+CREATE TABLE IF NOT EXISTS resources_by_nft_metadata (
+    metadata_cid TEXT NOT NULL,
+    resource_uri TEXT NOT NULL,
+
+    updated_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    inserted_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (metadata_cid,resource_uri)
 );
