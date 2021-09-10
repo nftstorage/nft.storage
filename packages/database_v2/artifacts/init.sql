b564644c6fe93a2f8475ddd7eeef185cf9544e4d
the-simian <harlinjesse@gmail.com>
2021-09-10 16:07:16 -0500

    adds remaining tables

--- packages/database_v2/artifacts/init.sql
+++ packages/database_v2/artifacts/init.sql
@@ -1,4 +1,4 @@
-DROP TYPE resource_status;
+DROP TYPE IF EXISTS resource_status CASCADE;
 CREATE TYPE resource_status AS ENUM (
     'Queued',
     'URIParseFailed',
@@ -7,7 +7,7 @@ CREATE TYPE resource_status AS ENUM (
     'ContentLinked'
 );
 
-DROP TYPE IF EXISTS nft_asset_status;
+DROP TYPE IF EXISTS nft_asset_status CASCADE;
 CREATE TYPE nft_asset_status AS ENUM (
     'Queued',
     'URIParseFailed',
@@ -17,7 +17,7 @@ CREATE TYPE nft_asset_status AS ENUM (
     'Linked'
 );
 
-DROP TYPE IF EXISTS pin_status;
+DROP TYPE IF EXISTS pin_status CASCADE;
 CREATE TYPE pin_status AS ENUM (
     'PinError',
     'PinQueued',
@@ -25,7 +25,7 @@ CREATE TYPE pin_status AS ENUM (
     'Pinning'
 );
 
-DROP TYPE IF EXISTS pin_service;
+DROP TYPE IF EXISTS pin_service CASCADE;
 CREATE TYPE pin_service AS ENUM (
     'Pinata',
     'IpfsCluster'
@@ -107,10 +107,62 @@ CREATE TABLE IF NOT EXISTS resource (
 );
 
 CREATE TABLE IF NOT EXISTS resources_by_nft_metadata (
-    metadata_cid TEXT NOT NULL,
+    nft_metadata_cid TEXT NOT NULL,
     resource_uri TEXT NOT NULL,
 
     updated_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     inserted_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (metadata_cid,resource_uri)
+    PRIMARY KEY (nft_metadata_cid,resource_uri)
+);
+
+CREATE TABLE IF NOT EXISTS content (
+    cid TEXT NOT NULL,
+    dag_size INTEGER,
+
+    updated_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    inserted_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (cid)
+);
+
+
+CREATE TABLE IF NOT EXISTS pin (
+    id BIGSERIAL NOT NULL,
+    content_cid TEXT NOT NULL,
+    service pin_service NOT NULL,
+    status pin_status NOT NULL,
+    status_text TEXT,
+
+    updated_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    inserted_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS blockchain_contract (
+    id TEXT NOT NULL,
+    name TEXT,
+    symbol TEXT,
+    supports_eip721_metadata BOOLEAN NOT NULL,
+
+    updated_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    inserted_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS erc721_import (
+    id TEXT NOT NULL,
+    next_id TEXT NOT NULL,
+    updated_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    inserted_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS erc721_import_by_nft (
+    erc721_import_id TEXT NOT NULL,
+    nft_id TEXT NOT NULL,
+    updated_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    inserted_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (erc721_import_id,nft_id)
 );
