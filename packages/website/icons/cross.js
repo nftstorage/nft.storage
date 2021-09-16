60499653c4ebb4ad4590527243a09bc8bb73c540
Rafael Ramalho <rafazelramalho19@gmail.com>
2021-09-16 18:05:19 +0100

    feat: add navbar and fix mobile navigation (#401)
    * feat: add navbar and fix mobile navigation
    * fix: header alignment in desktop

---
+++ packages/website/icons/cross.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+/**
+ * @param {any} props
+ */
+const Cross = (props) => (
+  <svg width="48" height="48" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M28.638 23.688l18.738 18.738-4.95 4.95-18.738-18.738L4.95 47.376 0 42.426l18.738-18.738L0 4.95 4.95 0l18.738 18.738L42.427 0l4.95 4.95-18.74 18.738z"
+    />
+  </svg>
+)
+
+export default Cross
