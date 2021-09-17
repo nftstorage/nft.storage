9b68fb17e240444c0d7e0ac2626f786717fe4724
Irakli Gozalishvili <contact@gozala.io>
2021-09-17 12:23:14 -0700

    feat: generalize migration scripts

---
+++ packages/niftysave/src/migrate/content.js
@@ -0,0 +1,45 @@
+import { script } from 'subprogram'
+import { writeMigrationState, start } from '../migrate.js'
+/**
+ * @typedef {{
+ *    cid: string
+ *    created?: import('faunadb').values.FaunaTime
+ *    dagSize?: number
+ * }} Content
+ *
+ * @param {{data: Content}} input
+ */
+const insert = ({ data }) => ({
+  cid: data.cid,
+  dag_size: data.dagSize || null,
+  inserted_at: (data.created || { date: new Date() }).date.toUTCString(),
+})
+
+/**
+ * @param {import('../migrate.js').Config} config
+ * @param {string|null} cursor
+ * @param {{data:Content}[]} input
+ */
+export const migrate = (config, cursor, input) =>
+  writeMigrationState(
+    config,
+    {
+      collection: 'Content',
+      cursor,
+      metadata: {},
+    },
+    {
+      insert_content: [
+        {
+          objects: input.map(insert),
+        },
+        {
+          affected_rows: 1,
+        },
+      ],
+    }
+  )
+
+const main = () => start({ collection: 'Content', migrate })
+
+script({ ...import.meta, main })
