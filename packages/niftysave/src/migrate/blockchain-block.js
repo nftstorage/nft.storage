28d2d71eb12834a65accc07d6385f65fc21317c9
the-simian <harlinjesse@gmail.com>
2021-09-20 13:51:19 -0500

    add number, needs better date

--- packages/niftysave/src/migrate/blockchain-block.js
+++ packages/niftysave/src/migrate/blockchain-block.js
@@ -14,6 +14,7 @@ import { script } from 'subprogram'
 const insert = ({ data }) => {
   return {
     hash: data.hash,
+    number: data.number,
     inserted_at: Migration.fromTimestamp(ts),
   }
 }
