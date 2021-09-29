743b1c73957b5e3d51643bb020c778ad649f5e42
Alan Shaw <alan.shaw@protocol.ai>
2021-09-29 11:08:54 +0100

    refactor: better table privileges for nft_storage
    Co-authored-by: Oli Evans <oli@tableflip.io>

--- packages/infra/heroku/grant-postgrest.sql
+++ packages/infra/heroku/grant-postgrest.sql
@@ -3,4 +3,7 @@ GRANT web_anon TO authenticator;
 -- NFT.Storage user
 GRANT nft_storage TO authenticator;
 GRANT USAGE ON SCHEMA public TO nft_storage;
-GRANT SELECT, UPDATE, DELETE ON public.aggregate, public.aggregate_entry, public.auth_key, public.content, public.deal, public.pin, public.upload, public.user TO nft_storage;
+-- allow access to all tables in the public schema currently
+GRANT SELECT, UPDATE ON ALL TABLES IN SCHEMA public TO nft_storage;
+-- allow access to new tables that are created in the public schema in the future
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, UPDATE ON TABLES TO nft_storage;
