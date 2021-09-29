be0f3cd9d1a2fdd2275d6dd9878c6765460c3dca
Alan Shaw <alan.shaw@protocol.ai>
2021-09-29 12:11:43 +0100

    fix: grant privs to sequences and functions to nft_storage

--- packages/infra/heroku/grant-postgrest.sql
+++ packages/infra/heroku/grant-postgrest.sql
@@ -3,7 +3,11 @@ GRANT web_anon TO authenticator;
 -- NFT.Storage user
 GRANT nft_storage TO authenticator;
 GRANT USAGE ON SCHEMA public TO nft_storage;
--- allow access to all tables in the public schema currently
+-- allow access to all tables/sequences/functions in the public schema currently
 GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA public TO nft_storage;
--- allow access to new tables that are created in the public schema in the future
+GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO nft_storage;
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO nft_storage;
+-- allow access to new tables/sequences/functions that are created in the public schema in the future
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE ON TABLES TO nft_storage;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO nft_storage;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT EXECUTE ON FUNCTIONS TO nft_storage;
