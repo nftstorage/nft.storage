6feb2b561b9d6397248e4f023c2f4d8e75937624
Alan Shaw <alan.shaw@protocol.ai>
2021-09-29 11:18:02 +0100

    fix: grant insert on tables

--- packages/infra/heroku/grant-postgrest.sql
+++ packages/infra/heroku/grant-postgrest.sql
@@ -4,6 +4,6 @@ GRANT web_anon TO authenticator;
 GRANT nft_storage TO authenticator;
 GRANT USAGE ON SCHEMA public TO nft_storage;
 -- allow access to all tables in the public schema currently
-GRANT SELECT, UPDATE ON ALL TABLES IN SCHEMA public TO nft_storage;
+GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA public TO nft_storage;
 -- allow access to new tables that are created in the public schema in the future
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, UPDATE ON TABLES TO nft_storage;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE ON TABLES TO nft_storage;
