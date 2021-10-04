fa850a5344f23cf8f520c85e982345b59f26f6b9
Alan Shaw <alan.shaw@protocol.ai>
2021-10-04 13:12:09 +0100

    feat: cron v1 (#496)
    Pins v1 adapted from https://github.com/ipfs-shipyard/nft.storage/pull/491/files#diff-62c0fbc347087401ac61e03bfc4a74bad04c74f27044e326da8a4db14fe354fc

--- packages/api/db/cargo.sql
+++ packages/api/db/cargo.sql
@@ -2,7 +2,7 @@ CREATE SCHEMA IF NOT EXISTS cargo;
 
 -- Import dag cargo schema
 IMPORT FOREIGN SCHEMA cargo
-    LIMIT TO (aggregate_entries, aggregates, deals)
+    LIMIT TO (aggregate_entries, aggregates, deals, dags)
     FROM SERVER dag_cargo_server
     INTO cargo;
 
