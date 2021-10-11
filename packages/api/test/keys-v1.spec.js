9ccec27be8c413b6d9372d83b5ae27ddc7ce53f2
Alan Shaw <alan.shaw@protocol.ai>
2021-10-11 14:16:55 +0100

    fix: auth keys tests

--- packages/api/test/keys-v1.spec.js
+++ packages/api/test/keys-v1.spec.js
@@ -14,7 +14,7 @@ describe('V1 - Auth Keys', () => {
   })
 
   it('should list just the default key', async () => {
-    const res = await fetch(`v1/internal/tokens`, {
+    const res = await fetch(`internal/tokens`, {
       headers: { Authorization: `Bearer ${client.token}` },
     })
     const { ok, value } = await res.json()
@@ -24,7 +24,7 @@ describe('V1 - Auth Keys', () => {
   })
 
   it('should create a key', async () => {
-    const res = await fetch(`v1/internal/tokens`, {
+    const res = await fetch(`internal/tokens`, {
       method: 'POST',
       headers: { Authorization: `Bearer ${client.token}` },
       body: JSON.stringify({ name: 'test2' }),
@@ -38,7 +38,7 @@ describe('V1 - Auth Keys', () => {
   })
 
   it('should error creating a key when name is not provided', async () => {
-    const res = await fetch(`v1/internal/tokens`, {
+    const res = await fetch(`internal/tokens`, {
       method: 'POST',
       headers: { Authorization: `Bearer ${client.token}` },
       body: JSON.stringify({}),
@@ -53,7 +53,7 @@ describe('V1 - Auth Keys', () => {
   })
 
   it('should delete a key', async () => {
-    const res = await fetch(`v1/internal/tokens`, {
+    const res = await fetch(`internal/tokens`, {
       method: 'POST',
       headers: { Authorization: `Bearer ${client.token}` },
       body: JSON.stringify({ name: 'test-delete' }),
@@ -61,7 +61,7 @@ describe('V1 - Auth Keys', () => {
     const { ok, value } = await res.json()
     assert.ok(ok, 'create key')
 
-    const resDelete = await fetch(`v1/internal/tokens`, {
+    const resDelete = await fetch(`internal/tokens`, {
       method: 'DELETE',
       headers: { Authorization: `Bearer ${client.token}` },
       body: JSON.stringify({ id: value.id }),
@@ -80,7 +80,7 @@ describe('V1 - Auth Keys', () => {
   })
 
   it('should error deleting a key when id is not provided', async () => {
-    const resDelete = await fetch(`v1/internal/tokens`, {
+    const resDelete = await fetch(`internal/tokens`, {
       method: 'DELETE',
       headers: { Authorization: `Bearer ${client.token}` },
       body: JSON.stringify({}),
@@ -95,7 +95,7 @@ describe('V1 - Auth Keys', () => {
 
   it('should not list deleted a keys', async () => {
     const client = await createClientWithUser()
-    const res1 = await fetch(`v1/internal/tokens`, {
+    const res1 = await fetch(`internal/tokens`, {
       method: 'POST',
       headers: { Authorization: `Bearer ${client.token}` },
       body: JSON.stringify({ name: 'test-key-1' }),
@@ -103,7 +103,7 @@ describe('V1 - Auth Keys', () => {
     const key1 = await res1.json()
     assert.ok(key1.ok, 'create key 1')
 
-    const res2 = await fetch(`v1/internal/tokens`, {
+    const res2 = await fetch(`internal/tokens`, {
       method: 'POST',
       headers: { Authorization: `Bearer ${client.token}` },
       body: JSON.stringify({ name: 'test-key-2' }),
@@ -111,7 +111,7 @@ describe('V1 - Auth Keys', () => {
     const key2 = await res2.json()
     assert.ok(res2.ok, 'create key 1')
 
-    const resDelete = await fetch(`v1/internal/tokens`, {
+    const resDelete = await fetch(`internal/tokens`, {
       method: 'DELETE',
       headers: { Authorization: `Bearer ${client.token}` },
       body: JSON.stringify({ id: key2.value.id }),
@@ -119,7 +119,7 @@ describe('V1 - Auth Keys', () => {
     const deleteData = await resDelete.json()
     assert.ok(deleteData.ok, 'delete key 2')
 
-    const res = await fetch(`v1/internal/tokens`, {
+    const res = await fetch(`internal/tokens`, {
       headers: { Authorization: `Bearer ${client.token}` },
     })
     const { ok, value } = await res.json()
