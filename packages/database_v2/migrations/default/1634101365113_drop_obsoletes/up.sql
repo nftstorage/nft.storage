0e0979e0b09dcb0efc47275c7146535fb7aaff7a
Irakli Gozalishvili <contact@gozala.io>
2021-10-12 22:36:00 -0700

    chore: remove obsolete code supportig niftysave migration (#594)

---
+++ packages/database_v2/migrations/default/1634101365113_drop_obsoletes/up.sql
@@ -0,0 +1,6 @@
+-- Cleanup obsolote tables, views, functions
+DROP VIEW nft_asset_view;
+DROP VIEW resource_view;
+DROP FUNCTION add_nft;
+DROP FUNCTION add_nft_metadata;
+DROP FUNCTION add_other_nft_resource;
