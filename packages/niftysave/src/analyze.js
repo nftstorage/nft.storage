nftstorage/nft.storage

commit d455b1f9228009065822e5bc52c8dddcf1033948

Feat/ingest change cursor to mint time (#601)
* feat: use mint_time as cursor for ingest
* chore: make cursor generic (#602), update types
* better paging on ingest w/cursor + offset.

Co-authored-by: Irakli Gozalishvili <contact@gozala.io>

packages/niftysave/src/analyze.js
@@ -1,15 +1,17 @@
-import * as Result from './result.js'
-import * as IPFSURL from './ipfs-url.js'
 import * as Cluster from './cluster.js'
+import * as Cursor from './hasura/cursor.js'
+import * as Hasura from './hasura.js'
 import * as IPFS from './ipfs.js'
+import * as IPFSURL from './ipfs-url.js'
+import * as Result from './result.js'
+
+import { exponentialBackoff, maxRetries, retry } from './retry.js'
 import { fetchResource, timeout } from './net.js'
+
+import { TransformStream } from './stream.js'
 import { configure } from './config.js'
 import { printURL } from './util.js'
 import { script } from 'subprogram'
-import * as Hasura from './hasura.js'
-import { exponentialBackoff, maxRetries, retry } from './retry.js'
-import * as Cursor from './hasura/cursor.js'
-import { TransformStream } from './stream.js'
 import { setTimeout as sleep } from './timers.js'
 
 export const main = async () => await spawn(await configure())
@@ -62,7 +64,7 @@ const spawn = async (config) => {
 const readInto = async (writable, config) => {
   const writer = writable.getWriter()
   try {
-    let cursor = Cursor.init()
+    let cursor = Cursor.init(new Date().toISOString())
 
     while (true) {
       console.log(
@@ -101,7 +103,7 @@ const readInto = async (writable, config) => {
 
         // Update cursor to point to the record after the last one.
         const lastRecord = /** @type {Asset} */ (page[page.length - 1])
-        cursor = Cursor.after(cursor, lastRecord)
+        cursor = Cursor.after(cursor, lastRecord.updated_at)
       }
     }
   } finally {
@@ -122,7 +124,7 @@ const readInto = async (writable, config) => {
  * @param {Object} config
  * @param {Hasura.Config} config.hasura
  * @param {number} config.batchSize
- * @param {Cursor.Cursor} cursor
+ * @param {Cursor.Cursor<string>} cursor
  * @returns {Promise<Asset[]>}
  */
 const fetchQueuedAssets = async (config, cursor) => {
@@ -134,7 +136,7 @@ const fetchQueuedAssets = async (config, cursor) => {
             _eq: 'Queued',
           },
           updated_at: {
-            _gte: cursor.updated_at,
+            _gte: cursor.time,
           },
         },
         order_by: [

packages/niftysave/src/pin.js
@@ -1,16 +1,18 @@
-import * as Result from './result.js'
-import * as IPFSURL from './ipfs-url.js'
+import * as CID from './cid.js'
 import * as Cluster from './cluster.js'
+import * as Cursor from './hasura/cursor.js'
+import * as Hasura from './hasura.js'
+import * as IPFSURL from './ipfs-url.js'
+import * as Result from './result.js'
+
+import { exponentialBackoff, maxRetries, retry } from './retry.js'
 import { fetchWebResource, timeout } from './net.js'
+
+import { TransformStream } from './stream.js'
 import { configure } from './config.js'
 import { printURL } from './util.js'
 import { script } from 'subprogram'
-import * as Hasura from './hasura.js'
-import { exponentialBackoff, maxRetries, retry } from './retry.js'
-import * as Cursor from './hasura/cursor.js'
-import { TransformStream } from './stream.js'
 import { setTimeout as sleep } from './timers.js'
-import * as CID from './cid.js'
 
 export const main = async () => await spawn(await configure())
 
@@ -63,7 +65,7 @@ const spawn = async (config) => {
 const readInto = async (writable, config) => {
   const writer = writable.getWriter()
   try {
-    let cursor = Cursor.init()
+    let cursor = Cursor.init(new Date().toISOString())
 
     while (true) {
       console.log(
@@ -102,7 +104,7 @@ const readInto = async (writable, config) => {
 
         // Update cursor to point to the record after the last one.
         const lastRecord = /** @type {Resource} */ (page[page.length - 1])
-        cursor = Cursor.after(cursor, lastRecord)
+        cursor = Cursor.after(cursor, lastRecord.updated_at)
       }
     }
   } finally {
@@ -123,7 +125,7 @@ const readInto = async (writable, config) => {
  * @param {Object} config
  * @param {Hasura.Config} config.hasura
  * @param {number} config.batchSize
- * @param {Cursor.Cursor} cursor
+ * @param {Cursor.Cursor<string>} cursor
  * @returns {Promise<Resource[]>}
  */
 const fetchQueuedResources = async (config, cursor) => {
@@ -135,7 +137,7 @@ const fetchQueuedResources = async (config, cursor) => {
             _eq: 'Queued',
           },
           updated_at: {
-            _gte: cursor.updated_at,
+            _gte: cursor.time,
           },
         },
         limit: config.batchSize,

packages/niftysave/src/hasura/cursor.js
@@ -19,15 +19,17 @@
  * Cursors for them would be represented as follows:
  *
  * ```js
- * { updated_at: '2021-10-01T22:16:21.369Z', offset: 0 } // data:a
- * { updated_at: '2021-10-01T22:16:21.369Z', offset: 1 } // data:b
- * { updated_at: '2021-10-01T22:16:21.369Z', offset: 2 } // data:c
- * { updated_at: '2021-10-04T22:17:26.934Z', offset: 0 } // data:d
- * { updated_at: '2021-10-04T22:17:26.934Z', offset: 1 } // data:e
+ * { time: '2021-10-01T22:16:21.369Z', offset: 0 } // data:a
+ * { time: '2021-10-01T22:16:21.369Z', offset: 1 } // data:b
+ * { time: '2021-10-01T22:16:21.369Z', offset: 2 } // data:c
+ * { time: '2021-10-04T22:17:26.934Z', offset: 0 } // data:d
+ * { time: '2021-10-04T22:17:26.934Z', offset: 1 } // data:e
  * ```
  *
+ *
+ * @template Time
  * @typedef {Object} Cursor
- * @property {DateISOString} updated_at - Timestamp of the record as an ISO string.
+ * @property {Time} time - Timestamp of the record as an ISO (string) or Unix Timestamp (number in seconds)
  * @property {number} offset - Number of records with matching `updated_at` to skip.
  */
 
@@ -35,12 +37,16 @@
  * Returns cursor representing UNIX epoch at offset 0. In practice this
  * implies starting from the very beginning.
  *
- * @returns {Cursor}
+ * @template Time
+ * @param {Time} time
+ * @returns {Cursor<Time>}
  */
-export const init = () => ({
-  updated_at: new Date(0).toISOString(),
-  offset: 0,
-})
+export const init = (time) => {
+  return {
+    time,
+    offset: 0,
+  }
+}
 
 /**
  * Advanced current `cursor` given a new `record`. New cursor
@@ -49,14 +55,31 @@ export const init = () => ({
  * Note: API assumes that state is fed records in a order, otherwise results
  * are unreliable.
  *
- * @param {Cursor} cursor
- * @param {{updated_at: DateISOString}} record
- * @returns {Cursor}
+ * @template Time
+ * @param {Cursor<Time>} cursor
+ * @param {Time} time
+ * @returns {Cursor<Time>}
  */
-export const after = (cursor, { updated_at }) =>
-  // If `updated_at` is the same inrease the offset by one, otherwise capture
-  // new `updated_at` and set offset to 1 so this record is skipped.
+export const after = (cursor, time) =>
+  // If `time` is the same inrease the offset by one, otherwise capture
+  // new `time` and set offset to 1 so this record is skipped.
 
-  cursor.updated_at === updated_at
-    ? { ...cursor, offset: cursor.offset + 1 }
-    : { updated_at, offset: 1 }
+  cursor.time === time
+    ? { time, offset: cursor.offset + 1 }
+    : { time, offset: 1 }
+
+/**
+ * @template Time
+ * @param {Cursor<Time>} cursor
+ */
+export const print = (cursor) => {
+  const { time, offset } = cursor
+  if (typeof time === 'number') {
+    const cursorAsDate = new Date(time * 1000).toUTCString()
+    console.log(
+      `┄\n👇 Cursor\t${time}\n👉 Offset\t${offset}\n🕰️ UTC\t${cursorAsDate}\n┄`
+    )
+  } else {
+    console.table(cursor)
+  }
+}

packages/niftysave/src/ingest.js
@@ -1,12 +1,13 @@
+import * as Cursor from './hasura/cursor.js'
 import * as ERC721 from '../gen/erc721/index.js'
 import * as Hasura from './hasura.js'
 
 import { exponentialBackoff, maxRetries, retry } from './retry.js'
-import { fetchNextNFTBatch, writeScrapedRecord } from './ingest/repo.js'
+import { fetchNFTBatch, writeScrapedRecord } from './ingest/repo.js'
 
 import { TransformStream } from './stream.js'
 import { configure } from './config.js'
-// import { intializeCursor } from './ingest/cursor.js'
+import { initIngestCursor } from './ingest/cursor.js'
 import { script } from 'subprogram'
 import { setTimeout as sleep } from './timers.js'
 
@@ -67,8 +68,11 @@ async function spawn(config) {
  */
 async function readIntoInbox(config, writeable) {
   const writer = writeable.getWriter()
-  //   let cursor = await intializeCursor(config)
-  let cursor = ''
+
+  let cursor = Cursor.init(await initIngestCursor(config))
+
+  Cursor.print(cursor)
+
   while (true) {
     let scrape = []
     try {
@@ -80,7 +84,7 @@ async function readIntoInbox(config, writeable) {
        * Eventually after enough failures, we can actually throw
        */
       scrape = await retry(
-        async () => fetchNextNFTBatch(config, cursor),
+        async () => fetchNFTBatch(config, cursor),
         [
           maxRetries(config.ingestScraperRetryLimit),
           exponentialBackoff(
@@ -98,12 +102,14 @@ async function readIntoInbox(config, writeable) {
         await sleep(config.ingestRetryThrottle)
       } else {
         await writer.ready
-        console.log(`📨  Adding ${scrape.length} items into Queue.`)
+        console.log(`📨 Adding ${scrape.length} items into Queue.`)
         for (const nft of scrape) {
           writer.write(nft)
           //Continuously update the in-memory cursor
-          cursor = nft.id
+          cursor = Cursor.after(cursor, parseInt(nft.mintTime))
         }
+        //Its useful to see what the cursor is periodically.
+        Cursor.print(cursor)
       }
     } catch (err) {
       console.error(`Something unexpected happened scraping nfts`, err)

packages/niftysave/src/ingest/cursor.js
@@ -11,30 +11,35 @@ import * as Hasura from '../hasura.js'
  * beginning because the db is empty
  * This is our starting cursor.
  * @param { Config } config
- * @returns { Promise<string>}
+ * @returns { Promise<number>}
  */
-export async function intializeCursor(config) {
-  const query = {
-    limit: 1,
-    order_by: [
-      {
-        inserted_at: Hasura.schema.order_by.desc,
-      },
-    ],
-  }
-
-  /**
-   * required type annotation due to design limitation in TS
-   * https://github.com/microsoft/TypeScript/issues/19360
-   * @type {{
-   *    id: true | undefined
-   *    updated_at: true | undefined
-   * }}
-   */
-  const resultsDefinition = { id: true, updated_at: true }
+export async function initIngestCursor(config) {
   const lastNFT = await Hasura.query(config.hasura, {
-    nft: [query, resultsDefinition],
+    nft: [
+      {
+        limit: 1,
+        where: {
+          inserted_at: {
+            _gte: new Date(new Date().toDateString()).toISOString(),
+          },
+        },
+        order_by: [
+          {
+            inserted_at: Hasura.schema.order_by.desc,
+          },
+        ],
+      },
+      { id: true, inserted_at: true, mint_time: true },
+    ],
   })
-
-  return lastNFT?.nft[0]?.id || '0'
+  console.log(`🪙 Init NFT\n${JSON.stringify(lastNFT, null, 2)}`)
+  /**
+   * You need to get the date in the database, or just start at the epoch,
+   * [ new Date(0) = Dec 31st 1969 whereas new Date() = now ]
+   * return the epoch { number } in UTC; getTime() always uses UTC
+   * ERC721 is in *seconds* JS is in *ms* so /1000
+   */
+  const mint_time = lastNFT?.nft[0]?.mint_time || 0
+  const cursor = Math.round(new Date(mint_time).getTime() / 1000)
+  return cursor
 }

packages/niftysave/src/ingest/repo.js
@@ -1,3 +1,4 @@
+import * as Cursor from './../hasura/cursor.js'
 import * as ERC721 from '../../gen/erc721/index.js'
 import * as Hasura from './../hasura.js'
 
@@ -26,11 +27,12 @@ import {
  * @param { ERC721ImportNFT } erc721Import
  */
 export async function writeScrapedRecord(config, erc721Import) {
-  console.log(`📥 Write ${erc721Import.id} to Hasura`)
+  const record = erc721ImportToNFTEndpoint(erc721Import)
+  console.log(`✍️ 🌿 ${record.mint_time.toUTCString()}\t🏷️ ${record.id}`)
   return Hasura.mutation(config.hasura, {
     ingest_erc721_token: [
       {
-        args: erc721ImportToNFTEndpoint(erc721Import),
+        args: record,
       },
       {
         id: true,
@@ -44,10 +46,10 @@ export async function writeScrapedRecord(config, erc721Import) {
  * Calls Subgraph and returns a batch of NFT records.
  * Hydrates the inbox.
  * @param { Config } config
- * @param { string } cursor
+ * @param { Cursor.Cursor<number> } cursor
  * @returns { Promise<ERC721ImportNFT[]> }
  */
-export async function fetchNextNFTBatch(config, cursor) {
+export async function fetchNFTBatch(config, cursor) {
   try {
     const nftsResult = await ERC721.query(
       config.erc721,
@@ -72,13 +74,15 @@ export async function fetchNextNFTBatch(config, cursor) {
  * If this is the first query, starting this module for the first time, the cursor
  * will be the id of whatever record was written last in our database.
  * @param {Config} config
- * @param {string} cursor
+ * @param {Cursor.Cursor<number>} cursor
  * @returns { ERC721.schema.QueryRequest }
  */
 const createSubgraphQuery = (config, cursor) => {
   const query = {
     first: config.ingestBatchSize,
-    where: { tokenURI_not: '', id_gt: cursor },
+    skip: cursor.offset,
+    where: { mintTime_gte: cursor.time.toString() },
+    orderBy: ERC721.schema.Token_orderBy.mintTime,
   }
   const erc721ResultDefinition = {
     id: 1,

commit f6c385f573a533dc29ffb971ea307c9c5d251981

fix: regression introduced by #601

packages/niftysave/src/analyze.js
@@ -65,7 +65,7 @@ const spawn = async (config) => {
 const readInto = async (writable, config) => {
   const writer = writable.getWriter()
   try {
-    let cursor = Cursor.init(new Date().toISOString())
+    let cursor = Cursor.init(new Date(0).toISOString())
 
     while (true) {
       console.log(
@@ -191,6 +191,7 @@ const analyzer = async (config, inbox) => {
   while (true) {
     console.log('📤 Pull nft asset from the queue')
     const next = await inbox.read()
+    console.log(next)
     if (next.done) {
       break
     } else {
@@ -360,17 +361,17 @@ const linkAsset = async (
 ) => {
   const resources = links.map((url) => linkResource(cid, url))
 
-  const { link_nft_asset } = await Hasura.mutation(
+  const { parse_nft_asset } = await Hasura.mutation(
     config.hasura,
     {
-      link_nft_asset: [
+      parse_nft_asset: [
         {
           args: {
             status,
             token_uri_hash: hash,
             status_text: statusText,
             ipfs_url: ipfsURL ? ipfsURL.href : null,
-            content_cid: cid,
+            metadata_cid: cid,
             dag_size: dagSize,
             // note need to use variable to workaround
             // https://github.com/graphql-editor/graphql-zeus/issues/144
@@ -391,7 +392,7 @@ const linkAsset = async (
     }
   )
 
-  return link_nft_asset
+  return parse_nft_asset
 }
 
 /**

packages/niftysave/src/pin-metadata.js
@@ -62,7 +62,7 @@ const spawn = async (config) => {
 const readInto = async (writable, config) => {
   const writer = writable.getWriter()
   try {
-    let cursor = Cursor.init(new Date().toISOString())
+    let cursor = Cursor.init(new Date(0).toISOString())
 
     while (true) {
       console.log(
@@ -251,6 +251,8 @@ const archive = async (config, asset) => {
  */
 
 const updateAsset = async (config, { hash, status, statusText }) => {
+  // TODO: Consider exposing more limited interface from sql function which
+  // upholds invariant of unidirectional status update.
   await Hasura.mutation(config.hasura, {
     update_nft_asset_by_pk: [
       {

packages/niftysave/src/pin.js
@@ -65,7 +65,7 @@ const spawn = async (config) => {
 const readInto = async (writable, config) => {
   const writer = writable.getWriter()
   try {
-    let cursor = Cursor.init(new Date().toISOString())
+    let cursor = Cursor.init(new Date(0).toISOString())
 
     while (true) {
       console.log(