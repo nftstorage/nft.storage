7e31fbf9ae82671816b676eb1bb96740d164c805
Alan Shaw <alan.shaw@protocol.ai>
2021-10-20 12:07:52 +0100

    feat: cron job to re-check failed pins (#631)

--- packages/cron/src/bin/pins-failed.js
+++ packages/cron/src/bin/pins-failed.js
@@ -2,17 +2,21 @@
 
 import dotenv from 'dotenv'
 import fetch from 'node-fetch'
-import { updatePendingPinStatuses } from '../jobs/pins-v1.js'
+import { checkFailedPinStatuses } from '../jobs/pins-v1.js'
 import { getDBClient, getCluster } from '../lib/utils.js'
 
 /** @ts-ignore */
 global.fetch = fetch
 
+const oneMonthAgo = () =>
+  new Date(new Date().setMonth(new Date().getMonth() - 1))
+
 async function main() {
   const db = getDBClient(process.env)
   const cluster = getCluster(process.env)
+  const after = process.env.AFTER ? new Date(process.env.AFTER) : oneMonthAgo()
 
-  await updatePendingPinStatuses({ db, cluster })
+  await checkFailedPinStatuses({ db, cluster, after })
 }
 
 dotenv.config()
