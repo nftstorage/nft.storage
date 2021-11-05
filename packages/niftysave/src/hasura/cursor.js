49066fdf491c9a3b43ca1647e4ddc71479497ef0
the-simian <harlinjesse@gmail.com>
2021-11-04 21:22:59 -0500

    fix: correct more imports for ingest after refactoring

--- packages/niftysave/src/hasura/cursor.js
+++ packages/niftysave/src/hasura/cursor.js
@@ -91,3 +91,11 @@ export const print = (cursor) => {
  */
 export const isDate = (date) =>
   date.length > 0 && new Date(date).toString() !== 'Invalid Date'
+
+/**
+ * @param {string} binStart
+ * @param {string} binEnd
+ * @returns {boolean}
+ */
+export const checkIsBinRange = (binStart, binEnd) =>
+  isDate(binEnd) && isDate(binStart)
