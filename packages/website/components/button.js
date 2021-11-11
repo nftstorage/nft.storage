ea7485213e940d608e5b21dfa94f7bce43fb735c
Jesse Harlin <harlinjesse@gmail.com>
2021-11-11 01:10:11 -0600

    Feat/doc styles (#759)
    * Adds accessible NFT-Storage swagger4.x theme
    Co-authored-by: marco <msmarc003@gmail.com>

--- packages/website/components/button.js
+++ packages/website/components/button.js
@@ -1,7 +1,7 @@
 import React, { useCallback } from 'react'
+
 import Link from 'next/link'
 import clsx from 'clsx'
-
 import countly from '../lib/countly'
 
 /**
--- packages/website/pages/api-docs.js
+++ packages/website/pages/api-docs.js
@@ -1,7 +1,7 @@
 // @ts-ignore
 import SwaggerUI from 'swagger-ui-react'
-import 'swagger-ui-react/swagger-ui.css'
 import { getToken } from '../lib/api'
+
 /**
  *
  * @param {Request} req
@@ -25,5 +25,11 @@ export function getStaticProps() {
 }
 
 export default function docs() {
-  return <SwaggerUI url="/schema.yml" requestInterceptor={requestHandler} />
+  return (
+    <SwaggerUI
+      url="/schema.yml"
+      requestInterceptor={requestHandler}
+      className="foo"
+    />
+  )
 }
