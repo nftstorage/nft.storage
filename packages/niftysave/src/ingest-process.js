2f95e0f9fca1ef2bfdc1c35fa0aa11a1e5530b18
the-simian <harlinjesse@gmail.com>
2021-11-14 21:01:45 -0600

    feat (niftysave): add entrypoint for long running process op on queue

---
+++ packages/niftysave/src/ingest-process.js
@@ -0,0 +1,44 @@
+import * as ERC721 from '../gen/erc721/index.js'
+import * as Hasura from './hasura.js'
+
+import { configure } from './config.js'
+import { script } from 'subprogram'
+
+/**
+ * @typedef {import('./ingest').ERC721ImportNFT} ERC721ImportNFT
+ * @typedef {import('./ingest').NFTSSubgraphResult} NFTSSubgraphResult
+ */
+
+/**
+ * @typedef { Object } Config
+ * @property { ERC721.Config } config.erc721
+ * @property { Hasura.Config } config.hasura
+ * @property { number } config.ingestRetryThrottle
+ * @property { number } config.ingestHighWatermark
+ * @property {string} ingestLastUpdatedDate
+ * @property {string} ingestRangeStartDate
+ * @property {string} ingestRangeEndDate
+ * @property { number } config.ingestScraperBatchSize
+ * @property { number } config.ingestScraperRetryLimit
+ * @property { number } config.ingestScraperRetryInterval
+ * @property { number } config.ingestScraperRetryMaxInterval
+ * @property { number } config.ingestWriterBatchSize
+ * @property { number } config.ingestWriterRetryLimit
+ * @property { number } config.ingestWriterRetryInterval
+ * @property { number } config.ingestWriterRetryMaxInterval
+ */
+
+export const main = async () => await spawn(await configure())
+
+/**
+ * Initialize and periodically run stored procedure to
+ * process records in the ingestion queue
+ * @param {Config} config
+ */
+async function spawn(config) {
+  console.log(`⏲️ Begin Processing Ingestion Queue.`)
+
+  return undefined
+}
+
+script({ ...import.meta, main })
