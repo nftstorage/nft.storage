52c3f619eb0530497c2db44957214f4c28f54c08
the-simian <harlinjesse@gmail.com>
2021-11-18 13:15:42 -0600

    fix (niftysave): batchify queue processor

--- packages/database_v2/migrations/default/1637256388437_batchify-ingest-processor/up.sql
+++ packages/database_v2/migrations/default/1637256388437_batchify-ingest-processor/up.sql
@@ -15,30 +15,27 @@ BEGIN
 END $$ LANGUAGE plpgsql;
 
 
-
-
 CREATE OR REPLACE FUNCTION ingest_unprocessed_tokens()
   RETURNS int
   AS $$
 DECLARE
   r record;
-  unprocessed_total integer;
-  batch_size integer :=100;
+  unprocessed_total integer :=0;
+  batch_size integer :=500;
 BEGIN
 
-    SELECT COUNT(*) 
-      FROM erc721_token_ingestion_queue 
-      WHERE last_processed 
+    SELECT COUNT(*)
+      FROM erc721_token_ingestion_queue
+      WHERE last_processed
       IS NULL
       INTO unprocessed_total;
-  
+
     WHILE (unprocessed_total > 0) LOOP
-  
+
     FOR r IN SELECT * FROM get_unprocessed_tokens_in_queue(batch_size) LOOP
          UPDATE erc721_token_ingestion_queue
            SET last_processed = now()
            WHERE id = r.id;
-         
          PERFORM public.ingest_erc721_token(
           r.id,
           r.token_id,
@@ -53,9 +50,8 @@ BEGIN
           r.owner_id,
           r.updated_at
          );
-         
     END LOOP;
-    
+
     unprocessed_total := unprocessed_total - batch_size;
   END LOOP;
   RETURN unprocessed_total;
