33010d53789f3da14dc48d74e939e5570c1eb551
the-simian <harlinjesse@gmail.com>
2021-12-02 21:55:04 -0600

    chore (niftysave): update deps and fix some type errors in the ipfs.js implementation of the ipfs-unixfs-importer

--- packages/niftysave/src/ipfs.js
+++ packages/niftysave/src/ipfs.js
@@ -1,16 +1,21 @@
 import * as IPFSURL from './ipfs-url.js'
-import { importer } from 'ipfs-unixfs-importer'
-import Multihash from 'multihashing-async'
+
+import IPLD from 'ipld'
 // @ts-expect-error - has no type defs
 import InMemory from 'ipld-in-memory'
-import pb from 'ipld-dag-pb'
-import IPLD from 'ipld'
+import Multihash from 'multihashing-async'
+import { importer } from 'ipfs-unixfs-importer'
 import multicodec from 'multicodec'
+import pb from 'ipld-dag-pb'
 
 /**
  * @typedef {Object} Config
  * @property {URL} url
  * @property {string} secret
+ */
+
+/**
+ * @typedef { import('ipfs-unixfs-importer').ImportResult } ImportResult
  */
 
 /**
@@ -50,7 +55,7 @@ export const cat = async (config, url, { v0 = false, signal } = {}) => {
 /**
  * @typedef {import('multiformats').CID} CID
  * @param {Blob} content
- * @returns {Promise<{cid:CID, size:number}>}
+ * @returns {Promise<ImportResult>}
  */
 export const importBlob = async (content) => {
   const buffer = await content.arrayBuffer()
