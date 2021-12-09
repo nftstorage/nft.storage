83aa78e91b41c214b91cec3ec6558c397113ee67
Alan Shaw <alan.shaw@protocol.ai>
2021-12-09 15:57:01 +0000

    fix: add missing grants to cargo schema

--- packages/infra/heroku/grant-postgrest.sql
+++ packages/infra/heroku/grant-postgrest.sql
@@ -11,3 +11,8 @@ GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO nft_storage;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE ON TABLES TO nft_storage;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO nft_storage;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT EXECUTE ON FUNCTIONS TO nft_storage;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE ON TABLES TO nft_storage;
+-- allow select on all tables in the cargo schema currently
+GRANT SELECT ON ALL TABLES IN SCHEMA cargo TO nft_storage;
+-- allow select on new tables that are created in the cargo schema in the future
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO nft_storage;
