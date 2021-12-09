b740fa1a70dff66680e2eee81fbb2eeb5bb54d2f
Alan Shaw <alan.shaw@protocol.ai>
2021-12-09 16:01:57 +0000

    fix: also grant access to schema

--- packages/infra/heroku/grant-postgrest.sql
+++ packages/infra/heroku/grant-postgrest.sql
@@ -12,7 +12,7 @@ ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE ON TABLES
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO nft_storage;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT EXECUTE ON FUNCTIONS TO nft_storage;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE ON TABLES TO nft_storage;
--- allow select on all tables in the cargo schema currently
+-- RO for cargo schema
+GRANT USAGE ON SCHEMA cargo TO nft_storage;
 GRANT SELECT ON ALL TABLES IN SCHEMA cargo TO nft_storage;
--- allow select on new tables that are created in the cargo schema in the future
 ALTER DEFAULT PRIVILEGES IN SCHEMA cargo GRANT SELECT ON TABLES TO nft_storage;
