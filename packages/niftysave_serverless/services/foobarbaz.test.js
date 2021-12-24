4aa853bae1163c63ed92d80889a607fee0324490
the-simian <harlinjesse@gmail.com>
2021-12-23 21:40:32 -0600

    chore: rename packages to make more sense

--- packages/niftysave_serverless/services/foobarbaz.test.js
+++
@@ -1,6 +0,0 @@
-const foobarbaz = require('./foobarbaz')
-
-test('example test', () => {
-  foobarbaz.jestTest().then(d => expect(d).toBe(3))
-  foobarbaz.jestTest().then(d => expect(d).toEqual(3))
-})
