87454f26da5ab8408dd026590ae6761158d0ec17
the-simian <harlinjesse@gmail.com>
2022-01-09 21:26:20 -0600

    chore: remove some unneeded files in the rewrite

--- packages/nifty_save/src/ingest/sources/subgraph.js
+++ packages/nifty_save/src/ingest/sources/subgraph.js
@@ -2,92 +2,59 @@ import { Cursor } from '../../cursor'
 import fetch from 'node-fetch'
 import { sleep } from '../../timers'
 
+//https://thegraph.com/hosted-service/subgraph/nftstorage/eip721-subgraph
+
 const INGEST_BATCH_SIZE = 1000
 const SUBGRAPH_URL =
   'https://api.thegraph.com/subgraphs/name/nftstorage/eip721-subgraph'
 
-/**
- * @param {Config} config
- * @param {Cursor.Cursor<number>} cursor
- */
-const createSubgraphQuery = cursor => {
-  const query = {
-    first: INGEST_BATCH_SIZE,
-    skip: cursor.offset,
-    where: { mintTime_gte: cursor.time.toString() },
-    orderBy: 'mintTime',
-  }
-  const erc721ResultDefinition = {
-    id: 1,
-    tokenID: 1,
-    tokenURI: 1,
-    mintTime: 1,
-    blockNumber: 1,
-    blockHash: 1,
-    contract: {
-      id: 1,
-      name: 1,
-      symbol: 1,
-      supportsEIP721Metadata: 1,
-    },
-    owner: {
-      id: 1,
-    },
-  }
-  return {
-    tokens: [query, erc721ResultDefinition],
+function makeTheGraphQuery(time, offset) {
+  return `
+{
+  tokens(
+    first: ${INGEST_BATCH_SIZE}
+    skip: ${offset}
+    orderBy: mintTime
+    where: { mintTime_gt: ${time} })
+  {
+    id
+    tokenID
+    contract {
+        id
+    }
+    tokenID
+    owner {
+        id
+    }
   }
 }
-
-// /**
-//  * READ
-//  * Calls Subgraph and returns a batch of NFT records.
-//  * Hydrates the inbox.
-//  * @param { Config } config
-//  * @param { Cursor.Cursor<number> } cursor
-//  */
-export async function fetchNFTBatch(config, cursor) {
-  console.log('call nft-batch')
-  try {
-    // const nftsResult = await ERC721.query(
-    //   SUBGRAPH_URL,
-    //   createSubgraphQuery(cursor)
-    // )
-    // if (nftsResult.ok === false) {
-    //   console.error(nftsResult)
-    //   throw new Error(JSON.stringify(nftsResult))
-    // }
-    // const { tokens } = nftsResult?.value || []
-    // console.log(`📤 Scraped ${tokens.length} nfts from Subgraph.`)
-    // return tokens.map(subgraphTokenToERC721ImportNFT)
-
-    const headers = {}
-    const nftResults = await fetch(SUBGRAPH_URL, {
-      method: 'POST',
-      headers: {
-        ...headers,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({}),
-    })
-
-    console.log(nftResults)
-  } catch (err) {
-    console.error(`🚨 Something unexpected happened scraping nfts`, err)
-    throw err
-  }
+`
 }
 
 export async function fetchNFTs(event, context) {
-  await sleep(500)
-
   const { detail } = event
 
-  const cursor = new Cursor(detail.rangeStartTime)
+  const cursor = new Cursor(detail.rangeStartTime / 1000)
+  const results = await fetch(SUBGRAPH_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: makeTheGraphQuery(cursor.time, cursor.offset),
+      variables: null,
+    }),
+  })
+
+  let nftBatch = await results.json()
+
+  console.log(nftBatch)
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'called Fetch NFTS from subgraph folder' }),
+    body: JSON.stringify({
+      message: `📤 Scraped ${nftBatch.length} nfts from Subgraph.`,
+    }),
   }
 }
 
