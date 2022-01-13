79e2016259237e5488d0fcdb6ecd3e31f4315bd3
the-simian <harlinjesse@gmail.com>
2022-01-12 21:37:23 -0600

    feat: better unit testing on the stack

--- packages/nifty_save/test/NiftySaveStack.test.js
+++ packages/nifty_save/test/NiftySaveStack.test.js
@@ -1,13 +1,33 @@
 import * as sst from '@serverless-stack/resources'
 
-import { expect, haveResource } from '@aws-cdk/assert'
+import {
+  countResources,
+  expect,
+  haveResource,
+  haveResourceLike,
+} from '@aws-cdk/assert'
 
 import NiftySaveStack from '../stacks/NiftySaveStack'
 
-test('Test Niftysave Stack', () => {
+//CFN resource types
+//https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-template-resource-type-ref.html
+
+test('Test Niftysave Stack - Global', () => {
   const app = new sst.App()
   // WHEN
-  const stack = new NiftySaveStack(app, 'nifty-save-stack')
+  const stack = new NiftySaveStack(app, 'niftysave-stack')
+
   // THEN
   expect(stack).to(haveResource('AWS::Lambda::Function'))
+  expect(stack).to(haveResource('AWS::ApiGatewayV2::Api'))
+  expect(stack).to(haveResource('AWS::Events::EventBus'))
+})
+
+test('NiftySave Stack - Injest', () => {
+  const app = new sst.App()
+  // WHEN
+  const stack = new NiftySaveStack(app, 'niftysave-stack')
+  expect(stack).to(haveResource('AWS::Lambda::Function'))
+  expect(stack).to(countResources('AWS::SQS::Queue', 2))
+  expect(stack).to(haveResource('AWS::Events::EventBus'))
 })
