36608f41463c909e199e650f4388ac4ce961fa30
the-simian <harlinjesse@gmail.com>
2022-01-14 23:11:57 -0600

    feat: more unit testing

---
+++ packages/nifty_save/src/dates.test.js
@@ -0,0 +1,19 @@
+import { checkIsBinRange, dateInSeconds, isDate } from './dates'
+
+import { textChangeRangeIsUnchanged } from 'typescript'
+
+test('Detects Date Correctly', () => {
+  const check = isDate('2020-2-1')
+  expect(check).toBe(true)
+})
+
+test('Detects Bin Range', () => {
+  const check = checkIsBinRange('2021-1-2', '2021-2-2')
+  expect(check).toBe(true)
+})
+
+test('Convert Date To seconds', () => {
+  const toSeconds = dateInSeconds('2021-1-1')
+  const asSeconds = 1609480800
+  expect(toSeconds).toBe(asSeconds)
+})
