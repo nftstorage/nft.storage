1cbccc521c636843ca18f0307c8f31d13b21c7d3
Daniel <daniel.ashcraft@ofashandfire.com>
2022-01-17 10:38:56 -0600

    updated title and mobile navigation

--- packages/website/theme.config.js
+++ packages/website/theme.config.js
@@ -10,9 +10,7 @@ export default {
   logo: <></>,
   head: (
     <>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="description" content="Nextra: the next docs builder" />
-      <meta name="og:title" content="Nextra: the next docs builder" />
+      <title>NFT.Storage Docs</title>
     </>
   ),
 }
