d069571f62c40e8b54ff2732d168f78a86d63298
Alan Shaw <alan.shaw@protocol.ai>
2022-01-17 18:20:15 +0000

    fix: failed pins cron

--- packages/cron/src/bin/pins-failed.js
+++ packages/cron/src/bin/pins-failed.js
@@ -5,7 +5,12 @@ import { fileURLToPath } from 'url'
 import dotenv from 'dotenv'
 import fetch from 'node-fetch'
 import { checkFailedPinStatuses } from '../jobs/pins.js'
-import { getDBClient, getCluster1, getCluster2 } from '../lib/utils.js'
+import {
+  getDBClient,
+  getCluster1,
+  getCluster2,
+  getCluster3,
+} from '../lib/utils.js'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 /** @ts-ignore */
@@ -18,9 +23,10 @@ async function main() {
   const db = getDBClient(process.env)
   const cluster1 = getCluster1(process.env)
   const cluster2 = getCluster2(process.env)
+  const cluster3 = getCluster3(process.env)
   const after = process.env.AFTER ? new Date(process.env.AFTER) : oneMonthAgo()
 
-  await checkFailedPinStatuses({ db, cluster1, cluster2, after })
+  await checkFailedPinStatuses({ db, cluster1, cluster2, cluster3, after })
 }
 
 dotenv.config({ path: path.join(__dirname, '../../../../.env') })
