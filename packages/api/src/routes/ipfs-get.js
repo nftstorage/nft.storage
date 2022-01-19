66ddc84890feed28ad5b23984e4618f940dbf47e
Vasco Santos <santos.vasco10@gmail.com>
2022-01-19 14:44:48 +0100

    fix: redirect

--- packages/api/src/routes/ipfs-get.js
+++ packages/api/src/routes/ipfs-get.js
@@ -1,14 +1,12 @@
 import { parseCid } from '../utils/utils.js'
-
-// TODO: Use prod gw
-const gateway = 'ipfs-staging.nft.storage'
+import { GATEWAY_NFT_STORAGE_HOSTNAME } from '../constants.js'
 
 /** @type {import('../bindings').Handler} */
 export const ipfsGet = async (event, { params }) => {
   // Parse and normalize CID
   const { contentCid } = parseCid(params.cid)
   const path = params.wild || ''
+  const url = `https://${contentCid}.${GATEWAY_NFT_STORAGE_HOSTNAME}/${path}`
 
-  const res = await fetch(`https://${contentCid}.${gateway}/${path}`)
-  return res
+  return Response.redirect(url, 302)
 }
