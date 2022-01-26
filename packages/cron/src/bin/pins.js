89262975e5d413f6afe5a9a8d65848aa3785f659
Alan Shaw <alan.shaw@protocol.ai>
2022-01-26 17:04:01 +0000

    refactor: use native postgres client in pins cron instead of postgrest

--- packages/cron/src/bin/pins.js
+++ packages/cron/src/bin/pins.js
@@ -5,24 +5,25 @@ import { fileURLToPath } from 'url'
 import dotenv from 'dotenv'
 import fetch from 'node-fetch'
 import { updatePendingPinStatuses } from '../jobs/pins.js'
-import {
-  getDBClient,
-  getCluster1,
-  getCluster2,
-  getCluster3,
-} from '../lib/utils.js'
+import { getPg, getCluster1, getCluster2, getCluster3 } from '../lib/utils.js'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 /** @ts-ignore */
 global.fetch = fetch
 
 async function main() {
-  const db = getDBClient(process.env)
-  const cluster1 = getCluster1(process.env)
-  const cluster2 = getCluster2(process.env)
-  const cluster3 = getCluster3(process.env)
+  const pg = getPg(process.env, 'rw')
+  await pg.connect()
 
-  await updatePendingPinStatuses({ db, cluster1, cluster2, cluster3 })
+  try {
+    const cluster1 = getCluster1(process.env)
+    const cluster2 = getCluster2(process.env)
+    const cluster3 = getCluster3(process.env)
+
+    await updatePendingPinStatuses({ pg, cluster1, cluster2, cluster3 })
+  } finally {
+    await pg.end()
+  }
 }
 
 dotenv.config({ path: path.join(__dirname, '../../../../.env') })
