34f0f7542abc1a4ae2566a149d9453089fd9dbf3
Vasco Santos <santos.vasco10@gmail.com>
2022-02-03 17:11:57 +0000

    fix: reset durable objects migrations (#1233)

--- packages/gateway/src/index.js
+++ packages/gateway/src/index.js
@@ -6,8 +6,8 @@ import { gatewayGet } from './gateway.js'
 import { metricsGet } from './metrics.js'
 
 // Export Durable Object namespace from the root module.
-export { GatewayMetrics2 } from './durable-objects/gateway-metrics.js'
-export { SummaryMetrics1 } from './durable-objects/summary-metrics.js'
+export { GatewayMetrics0 } from './durable-objects/gateway-metrics.js'
+export { SummaryMetrics0 } from './durable-objects/summary-metrics.js'
 export { CidsTracker0 } from './durable-objects/cids.js'
 export { GatewayRateLimits0 } from './durable-objects/gateway-rate-limits.js'
 
