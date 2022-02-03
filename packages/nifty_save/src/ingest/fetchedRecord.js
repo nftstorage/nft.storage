53352d6ce9906ed9c3bb74d0e4c8ea79868f8a77
the-simian <harlinjesse@gmail.com>
2022-02-03 01:16:36 -0600

    fix: fixes an issue where the dynamo table wasn't working because of permissions

--- packages/nifty_save/src/ingest/fetchedRecord.js
+++ packages/nifty_save/src/ingest/fetchedRecord.js
@@ -1,33 +1,23 @@
 import AWS from 'aws-sdk'
 import { sleep } from '../timers'
 
-const bus = new AWS.EventBridge()
 const dynamoDb = new AWS.DynamoDB.DocumentClient()
 
-function messageToRecord(msg) {
-  //tranformation may go here.
-  return {
-    ...msg,
-  }
-}
-
 export async function store(event) {
-  console.log(`Store fetched records called`)
-  const actualMessages = event.Records.map((x) => JSON.parse(x.body))
-  const records = actualMessages.map(messageToRecord)
+  const records = event.Records.map((x) => JSON.parse(x.body))
 
   let tableBatch = []
-
-  for (const record in records) {
-    tableBatch.push(
-      dynamoDb.put({
+  for (const record of records) {
+    const result = dynamoDb
+      .put({
         TableName: process.env.fetchedRecordsTableName,
         Item: {
           ...record,
           created_at: Date.now(),
         },
       })
-    )
+      .promise()
+    tableBatch.push(result)
   }
 
   await Promise.all(tableBatch)
