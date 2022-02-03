39f3a0c1212121a62260c480b197f1f52ef26b0b
Vasco Santos <santos.vasco10@gmail.com>
2022-02-03 17:21:49 +0100

    feat: track cached response times and summary histogram

--- packages/gateway/src/index.js
+++ packages/gateway/src/index.js
@@ -7,7 +7,7 @@ import { metricsGet } from './metrics.js'
 
 // Export Durable Object namespace from the root module.
 export { GatewayMetrics0 } from './durable-objects/gateway-metrics.js'
-export { SummaryMetrics0 } from './durable-objects/summary-metrics.js'
+export { SummaryMetrics1 } from './durable-objects/summary-metrics.js'
 export { CidsTracker0 } from './durable-objects/cids.js'
 export { GatewayRateLimits0 } from './durable-objects/gateway-rate-limits.js'
 
