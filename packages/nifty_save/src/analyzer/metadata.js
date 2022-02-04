f5c34ada59821f8d06d93f360afcd3f5feed3338
the-simian <harlinjesse@gmail.com>
2022-02-03 22:36:36 -0600

    feat: tokenuri detector is working

--- packages/nifty_save/src/analyzer/metadata.js
+++ packages/nifty_save/src/analyzer/metadata.js
@@ -8,6 +8,35 @@ export async function parse() {
   return {}
 }
 
+const httpsPrefix = 'https://'
+const ipfsPrefix = 'ipfs://'
+const dataPrefix = 'data:'
+const jsonPrefix = 'application/json'
+const base64Prefix = 'base64,'
+
 export function detectTokenURIFormat(tokenURI) {
+  if (tokenURI.startsWith(httpsPrefix)) {
+    return 'HTTPS'
+  }
+
+  if (tokenURI.startsWith(ipfsPrefix)) {
+    return 'IPFS'
+  }
+
+  const isData = tokenURI.startsWith(dataPrefix)
+
+  if (isData) {
+    const next = tokenURI.slice(dataPrefix.length)
+    const isJSON = next.startsWith(jsonPrefix)
+
+    if (isJSON) {
+      const next = tokenURI.slice(dataPrefix.length + jsonPrefix.length)
+      if (next.startsWith(`;${base64Prefix}`)) {
+        return 'BASE64'
+      }
+      return 'JSON'
+    }
+  }
+
   return 'UNKNOWN_FORMAT'
 }
