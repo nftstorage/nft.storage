3647f969be47ea2ccf758ab2aff263f635cb5c72
the-simian <harlinjesse@gmail.com>
2022-02-03 21:14:07 -0600

    fix: unbreak stack testing

--- packages/nifty_save/stacks/NiftySaveStack.test.js
+++ packages/nifty_save/stacks/NiftySaveStack.test.js
@@ -13,17 +13,17 @@ test('Test Niftysave Stack - Global', () => {
 
   console.log(template)
 
-  template.hasResource('AWS::Lambda::Function')
-  template.hasResource('AWS::ApiGatewayV2::Api')
-  template.hasResource('AWS::Events::EventBus')
+  template.hasResource('AWS::Lambda::Function', {})
+  template.hasResource('AWS::ApiGatewayV2::Api', {})
+  template.hasResource('AWS::Events::EventBus', {})
 })
 
-// test('NiftySave Stack - Injest', () => {
-//   const app = new sst.App()
-//   const stack = new NiftySaveStack(app, 'niftysave-stack')
-//   const template = Template.fromStack(stack)
+test('NiftySave Stack - Injest', () => {
+  const app = new sst.App()
+  const stack = new NiftySaveStack(app, 'niftysave-stack')
+  const template = Template.fromStack(stack)
 
-//   template.hasResource('AWS::Lambda::Function')
-//   template.resourceCountIs('AWS::SQS::Queue', 4)
-//   template.hasResource('AWS::Events::EventBus')
-// })
+  template.hasResource('AWS::Lambda::Function', {})
+  template.resourceCountIs('AWS::SQS::Queue', 4)
+  template.hasResource('AWS::Events::EventBus', {})
+})
