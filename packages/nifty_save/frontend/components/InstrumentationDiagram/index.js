7d9344b5185c0b3e81907eaf120787b72ce39103
the-simian <harlinjesse@gmail.com>
2022-02-07 10:27:53 -0600

    feat: organize instrumentation diagram into its own component

---
+++ packages/nifty_save/frontend/components/InstrumentationDiagram/index.js
@@ -0,0 +1,87 @@
+import { Canvas, Edge, Icon, Label, MarkerArrow, Node, Port } from 'reaflow'
+
+import { icons } from './icons'
+
+export default function InstrumentationDiagram(props) {
+  const { apiUrl } = props
+
+  const iconWH = { height: 30, width: 30 }
+
+  const nodes = [
+    {
+      id: 'api',
+      text: 'Niftysave Api Gateway',
+      icon: {
+        url: icons.gateway,
+        ...iconWH,
+      },
+    },
+    {
+      id: 'time-slicer',
+      text: 'Time Slicer',
+      icon: {
+        url: icons.lambda,
+        ...iconWH,
+      },
+    },
+    {
+      id: '3',
+      text: 'Thing',
+      icon: {
+        url: icons.lambda,
+        ...iconWH,
+      },
+    },
+  ]
+  const edges = [
+    {
+      id: 'api-to-timeslicer',
+      from: 'api',
+      to: 'time-slicer',
+    },
+  ]
+
+  const canvas = typeof window !== 'undefined' && (
+    <Canvas
+      nodes={nodes}
+      edges={edges}
+      node={
+        <Node
+          icon={<Icon />}
+          style={{ stroke: '#1a192b', fill: 'white', strokeWidth: 1 }}
+          label={<Label style={{ fill: 'black' }} />}
+          port={
+            <Port style={{ fill: 'blue', stroke: 'white' }} rx={10} ry={10} />
+          }
+        />
+      }
+    />
+  )
+
+  const sendTimeRangeToSlicer = async () => {
+    await fetch(`${apiUrl}/ingest/slice-queue/fill`, {
+      body: JSON.stringify({
+        timesliceSize: 6000000,
+        rangeStartTime: '2019-6-1',
+        rangeEndTime: '2019-6-2',
+        source: 'the-graph',
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    })
+  }
+
+  const diagram = (
+    <div style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}>
+      {canvas}
+    </div>
+  )
+
+  return (
+    <div>
+      <button onClick={sendTimeRangeToSlicer}>Send Timerange</button>
+    </div>
+  )
+}
--- packages/nifty_save/frontend/pages/index.js
+++ packages/nifty_save/frontend/pages/index.js
@@ -1,10 +1,4 @@
-import { Canvas, Edge, Icon, Label, MarkerArrow, Node, Port } from 'reaflow'
-
-import ApiGatewayIcon from 'react-aws-icons/dist/aws/logo/APIGateway'
 import Head from 'next/head'
-import SQSIcon from 'react-aws-icons/dist/aws/logo/SQS'
-import { icons } from '../components/icons'
-import styles from '../styles/Home.module.css'
 
 export async function getStaticProps() {
   console.log('url', process.env.NIFTYSAVE_API_URL)
@@ -17,65 +11,6 @@ export async function getStaticProps() {
 
 export default function Home(props) {
   const { NIFTYSAVE_API_URL } = props
-
-  const iconWH = { height: 30, width: 30 }
-
-  const nodes = [
-    {
-      id: 'api',
-      text: 'Niftysave Api Gateway',
-      icon: {
-        url: icons.gateway,
-        ...iconWH,
-      },
-    },
-    {
-      id: 'time-slicer',
-      text: 'Time Slicer',
-      icon: {
-        url: icons.lambda,
-        ...iconWH,
-      },
-    },
-    {
-      id: '3',
-      text: 'Thing',
-      icon: {
-        url: icons.lambda,
-        ...iconWH,
-      },
-    },
-  ]
-  const edges = [
-    {
-      id: 'api-to-timeslicer',
-      from: 'api',
-      to: 'time-slicer',
-    },
-  ]
-
-  const canvas = typeof window !== 'undefined' && (
-    <Canvas
-      nodes={nodes}
-      edges={edges}
-      node={
-        <Node
-          icon={<Icon />}
-          style={{ stroke: '#1a192b', fill: 'white', strokeWidth: 1 }}
-          label={<Label style={{ fill: 'black' }} />}
-          port={
-            <Port style={{ fill: 'blue', stroke: 'white' }} rx={10} ry={10} />
-          }
-        />
-      }
-    />
-  )
-
-  const diagram = (
-    <div style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}>
-      {canvas}
-    </div>
-  )
 
   const sendTimeRangeToSlicer = async () => {
     await fetch(
