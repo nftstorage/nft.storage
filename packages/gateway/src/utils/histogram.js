88840122cc088d3314027a230c3881e576d9dc4c
Vasco Santos <santos.vasco10@gmail.com>
2022-02-08 12:44:42 +0100

    fix: be more granular in response times

--- packages/gateway/src/utils/histogram.js
+++ packages/gateway/src/utils/histogram.js
@@ -1,5 +1,5 @@
 export const responseTimeHistogram = [
-  300, 500, 750, 1000, 1500, 2000, 3000, 5000, 10000, 20000,
+  50, 100, 200, 300, 500, 750, 1000, 1500, 2000, 3000, 5000, 10000, 20000,
 ]
 
 export function createResponseTimeHistogramObject() {
