5980e4797a3c391aa24ac3926d1ddea5005a3125
the-simian <harlinjesse@gmail.com>
2022-02-09 13:26:41 -0600

    feat: send slice instrumentation form

--- packages/nifty_save/frontend/components/InstrumentationDiagram/index.js
+++ packages/nifty_save/frontend/components/InstrumentationDiagram/index.js
@@ -1,3 +1,5 @@
+import Modal, { dialogModes } from '../Modal'
+
 import FlowDiagram from './FlowDiagram'
 import { sendTimeRangeToSlicer } from './actions'
 
@@ -15,12 +17,26 @@ export default function InstrumentationDiagram(props) {
         right: 0,
       }}
     >
-      <FlowDiagram
-        onClickApi={async () => {
-          console.log('clik')
-          await sendTimeRangeToSlicer(apiUrl, {})
-        }}
-      />
+      <Modal startOpen={true}>
+        <EditSliceCommandForm
+          onSubmit={async () => {
+            await sendTimeRangeToSlicer(apiUrl, {})
+          }}
+        />
+      </Modal>
+      <FlowDiagram onClickApi={() => {}} />
+    </div>
+  )
+}
+
+function EditSliceCommandForm(props) {
+  const { onSubmit } = props
+  return (
+    <div>
+      <p>asdasdasdasd</p>
+      <button className="btn hologram interactive" onClick={() => onSubmit()}>
+        Send Slice
+      </button>
     </div>
   )
 }
--- packages/nifty_save/frontend/components/Modal/index.js
+++ packages/nifty_save/frontend/components/Modal/index.js
@@ -1,19 +1,28 @@
-import './styles.css'
-
 import React, {
   forwardRef,
   useCallback,
   useEffect,
   useImperativeHandle,
+  useRef,
   useState,
 } from 'react'
 
 import { createPortal } from 'react-dom'
 
-const modalElement = document.getElementById('modal-root')
+export const dialogModes = {
+  MODAL: 'modal',
+  DIALOG: 'dialog',
+}
 
-export function Modal({ children, startOpen = false }, ref) {
+export function Modal(
+  { children, startOpen = false, mode = dialogModes.MODAL },
+  ref
+) {
   const [isOpen, setIsOpen] = useState(startOpen)
+  const [modalId, setModalId] = useState(
+    `modal_${Date.now()}_${Math.floor(Math.random() * 1000)}`
+  )
+  const [modalEl, setModalEl] = useState(null)
 
   const close = useCallback(() => setIsOpen(false), [])
 
@@ -34,29 +43,47 @@ export function Modal({ children, startOpen = false }, ref) {
   )
 
   useEffect(() => {
-    if (isOpen) document.addEventListener('keydown', onEsc, false)
+    function appendEl() {
+      if (!document.querySelector(`#${modalId}`)) {
+        let el = document.createElement('div')
+        el.setAttribute('id', modalId)
+        document.body.appendChild(el)
+      }
+      setTimeout(() => {
+        setModalEl(document.querySelector(`#${modalId}`))
+      })
+    }
+
+    if (isOpen) {
+      appendEl()
+      document.addEventListener('keydown', onEsc, false)
+    }
     return () => {
       document.removeEventListener('keydown', onEsc, false)
     }
-  }, [onEsc, isOpen])
+  }, [onEsc, isOpen, modalEl, modalId])
 
-  return createPortal(
-    isOpen ? (
-      <div className={`modal`}>
-        <div className="modal-overlay" onClick={close} />
+  const content = isOpen ? (
+    <div className={`modal`}>
+      <div className="modal-overlay" onClick={close} />
+
+      <div className="modal-body">
         <span
           role="button"
-          className="modal-close"
+          className="modal-close interactive"
           aria-label="close"
           onClick={close}
         >
           ✕
         </span>
-        <div className="modal-body">{children}</div>
+        <div className="modal-content">{children}</div>
       </div>
-    ) : null,
-    modalElement
-  )
+    </div>
+  ) : null
+
+  console.log(modalEl)
+
+  return modalEl ? createPortal(content, modalEl) : null
 }
 
 export default forwardRef(Modal)
