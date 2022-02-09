5e7951249c702aae602c72c8478cd269f7c51710
Daniel Ashcraft <daniel.ashcraft@ofashandfire.com>
2022-02-09 09:38:40 -0600

    fix:add redirect rule for docs (#1309)
    * fix:add redirect rule for docs
    
    * fix: try redirects instead of path maps
    
    * test another path map route
    
    * added all the routes to the export path map
    
    * force cloudflare rebuild
    
    * fix sidebar and remove trailing slash for redirects
    
    * missed troubleshooting
    
    * added export paths for root elements to prevent breaking page

--- packages/website/next.config.js
+++ packages/website/next.config.js
@@ -20,6 +20,31 @@ const nextConfig = withBundleAnalyzer({
   exportPathMap: async function () {
     return {
       '/ipfs-404.html': { page: '/404' },
+      '/docs/': { page: '/docs/quickstart', statusCode: 301 },
+      '/docs/why-nft-storage': { page: '/docs/why-nft-storage' },
+      '/docs/troubleshooting': { page: '/docs/troubleshooting' },
+      '/docs/quickstart': { page: '/docs/quickstart' },
+      '/docs/concepts/decentralized-storage': {
+        page: '/docs/concepts/decentralized-storage',
+      },
+      '/docs/concepts': {
+        page: '/docs/concepts/decentralized-storage',
+      },
+      '/docs/concepts/architecture-considerations': {
+        page: '/docs/concepts/architecture-considerations',
+      },
+      '/docs/concepts/car-files': { page: '/docs/concepts/car-files' },
+      '/docs/how-to/mint-erc-1155': { page: '/docs/how-to/mint-erc-1155' },
+      '/docs/how-to': {
+        page: '/docs/how-to/mint-erc-1155',
+      },
+      '/docs/how-to/mint-custom-metadata': {
+        page: '/docs/how-to/mint-custom-metadata',
+      },
+      '/docs/how-to/retrieve': { page: '/docs/how-to/retrieve' },
+      '/docs/client': { page: '/docs/client/api-docs' },
+      '/docs/client/api-docs': { page: '/docs/client/api-docs' },
+      '/docs/client/lib': { page: '/docs/client/lib' },
     }
   },
 })
