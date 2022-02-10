6a341f8070c8152a5833588d7bddd24eff87d784
ice.breaker <ice.breaker@trigram.co>
2022-02-09 18:00:05 -0600

    feat: Add better report route, setup queries to cloudwatch and return them as json.

---
+++ packages/nifty_save/src/report/common.js
@@ -0,0 +1,17 @@
+/**
+ * Build a query for a metric on cloudwatch.
+ * @param {string} namespace - The namespace of the metric to get (AWS/SQS, AWS/DynamoDB, AWS/Lambda).
+ * @param {string} name - The name of the metric to get.
+ * @param {string} [method] - The method to apply to the metric (Sum, Average, Maximum, etc)
+ * @returns {object} The metric query.
+ */
+export function buildMetricQuery(namespace, name, method = 'Sum') {
+  const projectName = process.env.projectName
+  const period = 120 // time sample period in seconds.
+
+  return {
+    Id: `report_health_${name.toLowerCase()}_${method.toLowerCase()}`,
+    Expression: `SEARCH('${namespace} "${name}" AND (${projectName})', '${method}', ${period})`,
+    ReturnData: true,
+  }
+}
