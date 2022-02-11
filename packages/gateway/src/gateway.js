cf841bbf0c2e24d303dae05f494b1b4fcbc500bd
Vasco Santos <santos.vasco10@gmail.com>
2022-02-11 10:12:07 +0000

    fix: gateway requested with custom header requested from (#1344)

--- packages/gateway/src/gateway.js
+++ packages/gateway/src/gateway.js
@@ -236,6 +236,7 @@ function getHeaders(request) {
     'X-Forwarded-For': `${request.headers.get(
       'cf-connecting-ip'
     )}${existingProxies}`,
+    'X-Forwarded-Host': request.headers.get('host'),
   }
 }
 
