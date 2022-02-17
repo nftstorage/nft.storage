1173774428781b35ecae706f7baf29fe968bcadd
Adam Munns <hello@munnsmedia.com>
2022-02-17 13:25:48 -0800

    feat: add form for pinning request

--- packages/website/pages/manage.js
+++ packages/website/pages/manage.js
@@ -2,7 +2,7 @@ import { Else, If, Then, When } from 'react-if'
 import { deleteToken, getTokens } from '../lib/api'
 import { useEffect, useState } from 'react'
 import { useQuery, useQueryClient } from 'react-query'
-
+import { Dialog } from '@headlessui/react'
 import Button from '../components/button.js'
 import Loading from '../components/loading.js'
 import countly from '../lib/countly.js'
@@ -31,9 +31,11 @@ export function getStaticProps() {
 export default function ManageKeys({ user }) {
   const [deleting, setDeleting] = useState('')
   const [copied, setCopied] = useState('')
+  const [pinReqestOpen, setPinReqestOpen] = useState(false)
   const queryClient = useQueryClient()
   const { status, data } = useQuery('get-tokens', () => getTokens(), {
     enabled: !!user,
+    refetchOnWindowFocus: false,
   })
   useEffect(() => {
     if (!copied) return
@@ -90,6 +92,13 @@ export default function ManageKeys({ user }) {
           <Else>
             <div className="flex items-center mb3">
               <h1 className="flex-auto chicagoflf mv4">API Keys</h1>
+              <Button
+                className="flex-none mr3"
+                id="request-api-pinning"
+                onClick={() => setPinReqestOpen(true)}
+              >
+                Request API Pinning Access
+              </Button>
               <Button
                 href={{
                   pathname: '/new-key',
@@ -185,6 +194,113 @@ export default function ManageKeys({ user }) {
           </Else>
         </If>
       </div>
+
+      <Dialog
+        open={pinReqestOpen}
+        onClose={() => setPinReqestOpen(false)}
+        className="modal swagger-ui"
+      >
+        <div className="flex items-center justify-center min-vh-100">
+          <Dialog.Overlay className="fixed inset-0 bg-black-0 min-vh-100 w-100" />
+          <div className="modal-content relative bg-white max-w-sm mx-auto pa4">
+            <button
+              className="modal-close absolute chicagoflf top-0 right-0"
+              onClick={() => setPinReqestOpen(false)}
+            >
+              <svg
+                width="20px"
+                height="20px"
+                viewBox="0 0 12 12"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g transform="matrix(1.2,0,0,1.2,0,0)">
+                  <path
+                    d="M6.5 3.5L3.5 6.5"
+                    fill="none"
+                    stroke="#000000"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></path>
+                  <path
+                    d="M3.5 3.5L6.5 6.5"
+                    fill="none"
+                    stroke="#000000"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></path>
+                  <path
+                    d="M0.500 0.500 L9.500 0.500 L9.500 9.500 L0.500 9.500 Z"
+                    fill="none"
+                    stroke="#000000"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></path>
+                </g>
+              </svg>
+            </button>
+            <Dialog.Title className={`chicagoflf`}>
+              Request API Pinning Allowlist Access
+            </Dialog.Title>
+            <Dialog.Description>
+              {/* This will permanently deactivate your account */}
+              <form>
+                <p className="mv4">
+                  <label htmlFor="user-pin-request-why" className="f5 dib mb2">
+                    Why you are looking for pinning service API access?{' '}
+                    <small className="db black-50 mt1">
+                      (e.g. you are an artist looking for extra redundancy)
+                    </small>
+                  </label>
+                  <input
+                    type="text"
+                    id="user-pin-request-why"
+                    name="user-pin-request-why"
+                    className="w-100 db ba b--black w5 pa2"
+                  />
+                </p>
+                <p className="mv4">
+                  <label htmlFor="user-pin-request-cids" className="f5 dib mb2">
+                    What metadata you are looking to pin?
+                    <small className="db black-50 mt1">
+                      (5-10 CIDs of NFTs)
+                    </small>
+                  </label>
+                  <textarea
+                    id="user-pin-request-cids"
+                    name="user-pin-request-cids"
+                    className="w-100 db ba b--black w5 pa2 textarea"
+                  ></textarea>
+                </p>
+                <p className="mv4">
+                  <label
+                    htmlFor="user-pin-request-social"
+                    className="f5 dib mb2"
+                  >
+                    Your profile on an NFT service (artist profile, collector,
+                    etc.)
+                  </label>
+                  <input
+                    type="text"
+                    id="user-pin-request-social"
+                    name="user-pin-request-social"
+                    className="w-100 db ba b--black w5 pa2"
+                  />
+                </p>
+                <p className="mt4 ml1 flex">
+                  <Button
+                    className="bg-nslime mt4"
+                    type="submit"
+                    id="create-new-key"
+                    onClick={() => setPinReqestOpen(false)}
+                  >
+                    Submit Request
+                  </Button>
+                </p>
+              </form>
+            </Dialog.Description>
+          </div>
+        </div>
+      </Dialog>
     </main>
   )
 }
