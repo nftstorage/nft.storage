c3f73a792b864e00cda96a54f85998dc89842453
Daniel Ashcraft <daniel.ashcraft@ofashandfire.com>
2022-03-02 20:07:13 -0600

    feat: Added support for full path external redirects (#1526)
    * Added support for full path external redirects
    
    * Add prop defs for external redirect
    
    * added url to dependency list
    
    * window.location was incorrect, needed href instead
    
    * Updated to show full api docs
    
    * add redirect
    
    * Update copy
    
    * fix: redirect for generated link

--- packages/website/next.config.js
+++ packages/website/next.config.js
@@ -45,6 +45,8 @@ const nextConfig = withBundleAnalyzer({
       '/docs/how-to/retrieve': { page: '/docs/how-to/retrieve' },
       '/docs/how-to/store-directory': { page: '/docs/how-to/store-directory' },
       '/docs/client': { page: '/docs/client/js' },
+      '/docs/client/https': { page: '/docs/client/http' },
+      '/docs/client/generated': { page: '/docs/client/generated' },
       '/docs/client/js': { page: '/docs/client/js' },
       '/docs/client/api-docs': { page: '/docs/client/js' },
       '/docs/client/lib': { page: '/docs/client/js' },
