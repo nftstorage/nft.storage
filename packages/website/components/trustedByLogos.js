db624711f10b672a3422e149092f4df0a5495d91
Adam Munns <hello@munnsmedia.com>
2022-03-09 14:46:16 -0800

    feat: add stats page (#1376)
    * rebase main
    
    * Adding placeholder stats
    
    * Updated stats js to start having real stats
    
    * Added api endpoint as well as tests for the aggregate, created a simple dirty pipeline for decorating the calculated values on the object
    
    * Updated lint commands as well as test commands
    
    * What they called tests, was actually lint and ts build, not tests per se, updated script to run lint and not tests
    
    * Made a handful of changes for formatting as well as data that is currently available, removed week over week for nft deals, used total deals and deal size
    
    * stats utils test updates
    
    * yarn lock
    
    * add stats route to navbar
    
    * Added typedefs, cleared typedoc issues, added fix for client lib ts lint error
    
    * remove test and change back to lint, will update in new pr
    
    Co-authored-by: Daniel <daniel.ashcraft@ofashandfire.com>

---
+++ packages/website/components/trustedByLogos.js
@@ -0,0 +1,31 @@
+/**
+ * Logo Component
+ * @param {Object} props
+ * @param {string} props.src
+ */
+const Logo = ({ src }) => (
+  <img
+    className="marketplace-logo"
+    src={`/images/marketplace-logos/${src}`}
+    alt="Nft.Storage Users"
+  />
+)
+/**
+ * Logos Component
+ * @param {Object} props
+ * @param {string[]} props.logos
+ *
+ */
+export const TrustedBy = ({ logos }) => {
+  return (
+    <div className="marketplace-logos-container center pv4 ph3 ph5-ns">
+      <h2 className="tc mt0 chicagoflf">Trusted by</h2>
+      <div className="marketplace-logo-grid">
+        {logos.map((logo) => (
+          <Logo key={`marketplace-logo-${logo}`} src={logo} />
+        ))}
+      </div>
+      <p className="tc chicagoflf">and 20,000+ other users!</p>
+    </div>
+  )
+}
