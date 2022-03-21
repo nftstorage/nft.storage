eafbef666dfde84e384ee139d4eb6f339f629be7
Hugo Dias <hugomrdias@gmail.com>
2022-03-21 11:52:30 +0000

    chore: add readme and comments

--- examples/ucan-node/index.js
+++ examples/ucan-node/index.js
@@ -4,17 +4,15 @@ import { fetch } from 'undici'
 import { KeyPair } from 'ucan-storage/keypair'
 import { build } from 'ucan-storage/ucan-storage'
 
-/**
- * npx ucan-storage keypair
- *
- * DID:           did:key:z6Mkiim1bwsGbxGs1upx8afVSN92rYq4UzZ7vPFNcNFcnphm
- * Public Key:    P2c2JFGWmiBczwBSK/uEl5Dt8wMnqLoWGThGuBZf15A=
- * Private Key:   Iq6QntRbBtVUNjpolb2fr4IkdG5VpCRC4ve6XI2Prk0=
- */
-
 const endpoint = 'https://api.nft.storage' // the default
 const token = process.env.API_KEY // your API key from https://nft.storage/manage
+const privateKey = process.env.PRIVATE_KEY // Generate it with `npx ucan-storage keypair`
 
+/**
+ * Register you DID with the service
+ *
+ * @param {string} did
+ */
 async function registerDID(did) {
   const registerRes = await fetch(new URL('/user/did', endpoint), {
     method: 'POST',
@@ -30,7 +28,14 @@ async function registerDID(did) {
   }
 }
 
-async function getUCAN(kp, serviceDID) {
+/**
+ * Request a Root UCAN from the service
+ *
+ * @param {KeyPair} kp - Your own keypair
+ * @param {string} serviceDID - Service DID
+ * @returns
+ */
+async function requestRootUCAN() {
   const ucanReq = await fetch(new URL('/ucan/token', endpoint), {
     method: 'POST',
     headers: {
@@ -43,6 +48,18 @@ async function getUCAN(kp, serviceDID) {
 
   const { value: rootUCAN } = await ucanReq.json()
 
+  return rootUCAN
+}
+
+/**
+ * Sign a new request UCAN to do an upload
+ *
+ * @param {KeyPair} kp
+ * @param {string}} serviceDID
+ * @param {string} rootUCAN
+ * @returns
+ */
+async function signRequestUCAN(kp, serviceDID, rootUCAN) {
   return await build({
     issuer: kp,
     audience: serviceDID,
@@ -58,15 +75,20 @@ async function getUCAN(kp, serviceDID) {
 }
 
 async function main() {
-  const kp = await KeyPair.fromExportedKey(
-    'Iq6QntRbBtVUNjpolb2fr4IkdG5VpCRC4ve6XI2Prk0='
-  )
+  const kp = await KeyPair.fromExportedKey(privateKey)
+
+  // Get Service DID
   const didRes = await fetch(new URL('/did', endpoint))
   const { ok, value: serviceDID } = await didRes.json()
 
+  // Register your DID generated from the Keypair and request Root UCAN.
+  // You only need to do this once, if you already have a keypair and a Root UCAN
+  // you can just save it in your system and re-use it for the next step.
   await registerDID(kp.did())
+  const rootUCAN = await requestRootUCAN()
 
-  const ucan = await getUCAN(kp, serviceDID)
+  // Sign a new request UCAN
+  const ucan = await signRequestUCAN(kp, serviceDID, rootUCAN)
 
   const storage = new NFTStorage({ endpoint, token: ucan })
   const data = await fs.promises.readFile('pinpie.jpg')
