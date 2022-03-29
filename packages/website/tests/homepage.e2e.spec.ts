ccb3c8e47d541b8e6b8e5c50510eceadcb4664dd
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2022-03-29 14:38:07 -0700

    ci: add @playwright/test POC

---
+++ packages/website/tests/homepage.e2e.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect, Page } from '@playwright/test'
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/')
+})
+
+test.describe('nft.storage homepage', () => {
+  test('should explain the product', async ({ page }) => {
+    const nftStorage = await page.locator('.nft-storage')
+    expect(await nftStorage.innerText()).toMatch(/Free Storage for NFTs/gi)
+  })
+  test.skip('should have title', async ({ page }) => {
+    expect(page).toHaveTitle(/^NFT\.Storage/i)
+  })
+})
