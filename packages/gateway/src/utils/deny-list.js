a74697b4a0e6c11df6ec172f5193cb837ba6b670
Vasco Santos <santos.vasco10@gmail.com>
2022-04-05 17:14:23 +0200

    feat: remove gateway package

--- packages/gateway/src/utils/deny-list.js
+++
@@ -1,8 +0,0 @@
-import * as uint8arrays from 'uint8arrays'
-import { sha256 } from 'multiformats/hashes/sha2'
-
-export async function toDenyListAnchor(cid) {
-  const multihash = await sha256.digest(uint8arrays.fromString(`${cid}/`))
-  const digest = multihash.bytes.subarray(2)
-  return uint8arrays.toString(digest, 'hex')
-}
