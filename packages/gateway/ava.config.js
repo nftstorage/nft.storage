53249159f5682866019cddacb930c89fe790d18c
Vasco Santos <santos.vasco10@gmail.com>
2022-04-06 16:32:38 +0200

    feat: remove gateway package (#1769)

--- packages/gateway/ava.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  nonSemVerExperiments: {
-    configurableModuleFormat: true,
-  },
-  files: ['test/*.spec.js'],
-  timeout: '5m',
-  concurrency: 1,
-  nodeArguments: ['--experimental-vm-modules'],
-}
