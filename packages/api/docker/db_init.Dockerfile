56e44ae29aba7e1a69acdd7bffba8cb379e1c96b
redaphid <iam@aaronherres.com>
2022-04-11 14:50:53 -0700

    Docker can run db-init script. It doesn't work, but it can run it

---
+++ packages/api/docker/db_init.Dockerfile
@@ -0,0 +1,23 @@
+#
+# Docker NodeJS Typescript Starter
+# Example Dockerfile
+#
+FROM node:16-alpine3.12
+# Intall missing dependencies
+RUN apk add --update bash
+# Create App dir
+RUN mkdir -p /app
+
+# Set working directory to App dir
+WORKDIR /app
+
+# Copy project files
+COPY ./package.json .
+RUN yarn install
+
+COPY . .
+# Install dependencies
+
+RUN yarn install
+
+ENTRYPOINT "./scripts/cli.js --db-sql init"
--- packages/api/scripts/cli.js
+++ packages/api/scripts/cli.js
@@ -8,7 +8,6 @@ import { build } from 'esbuild'
 import Sentry from '@sentry/cli'
 import { createRequire } from 'module'
 // @ts-ignore
-import git from 'git-rev-sync'
 import { dbCmd } from './cmds/db.js'
 import { dbSqlCmd } from './cmds/db-sql.js'
 import { dbTypesCmd } from './cmds/db-types.js'
@@ -42,60 +41,6 @@ const PluginAlias = {
   },
 }
 prog
-  .command('build')
-  .describe('Build the worker.')
-  .option('--env', 'Environment', 'dev')
-  .action(async (opts) => {
-    try {
-      const version = `${pkg.name}@${pkg.version}-${opts.env}+${git.short(
-        __dirname
-      )}`
-      await build({
-        entryPoints: [path.join(__dirname, '../src/index.js')],
-        bundle: true,
-        outfile: 'dist/worker.js',
-        legalComments: 'external',
-        inject: [path.join(__dirname, 'node-globals.js')],
-        plugins: [PluginAlias],
-        define: {
-          VERSION: JSON.stringify(version),
-          COMMITHASH: JSON.stringify(git.long(__dirname)),
-          BRANCH: JSON.stringify(git.branch(__dirname)),
-          global: 'globalThis',
-        },
-        minify: opts.env === 'dev' ? false : true,
-        sourcemap: true,
-      })
-
-      // Sentry release and sourcemap upload
-      if (process.env.SENTRY_UPLOAD === 'true') {
-        const cli = new Sentry(undefined, {
-          authToken: process.env.SENTRY_TOKEN,
-          org: 'protocol-labs-it',
-          project: 'api',
-          dist: git.short(__dirname),
-        })
-
-        await cli.releases.new(version)
-        await cli.releases.setCommits(version, {
-          auto: true,
-          ignoreEmpty: true,
-          ignoreMissing: true,
-        })
-        await cli.releases.uploadSourceMaps(version, {
-          include: ['./dist'],
-          urlPrefix: '/',
-        })
-        await cli.releases.finalize(version)
-        await cli.releases.newDeploy(version, {
-          env: opts.env,
-        })
-      }
-    } catch (err) {
-      console.error(err)
-      process.exit(1)
-    }
-  })
   .command('db-sql')
   .describe('Database scripts')
   .option('--reset', 'Reset db before running SQL.', false)
