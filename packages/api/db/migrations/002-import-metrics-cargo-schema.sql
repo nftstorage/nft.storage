d9596643f6c61434ab1d6e5eeb41a468c0870be6
Daniel <daniel.ashcraft@ofashandfire.com>
2022-04-27 08:39:13 -0500

    Updated migration to only import tables that are new

--- packages/api/db/migrations/002-import-metrics-cargo-schema.sql
+++ packages/api/db/migrations/002-import-metrics-cargo-schema.sql
@@ -1,5 +1,5 @@
 -- Import dag cargo schema
 IMPORT FOREIGN SCHEMA cargo
-    LIMIT TO (aggregate_entries, aggregates, deals, dags)
+    LIMIT TO (metrics, metrics_log)
     FROM SERVER dag_cargo_server
     INTO cargo;
