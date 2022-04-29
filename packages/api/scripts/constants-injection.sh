8fa813960927f07bf2597f4940b7dfa76defba80
redaphid <iam@aaronherres.com>
2022-04-28 21:01:43 -0700

    trying unsucessfully to use host env vars.

--- packages/api/scripts/constants-injection.sh
+++ packages/api/scripts/constants-injection.sh
@@ -3,20 +3,19 @@ mkdir -p /app/dist && \
 node_modules/esbuild/bin/esbuild \
   --bundle \
   --sourcemap \
-  --define:PRIVATE_KEY='"xmbtWjE9eYuAxae9G65lQSkw36HV6H+0LSFq2aKqVwY="' \
-  --define:DATABASE_CONNECTION='"postgresql://postgres:postgres@db:5432/postgres"' \
-  --define:DATABASE_TOKEN='"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTYwMzk2ODgzNCwiZXhwIjoyNTUwNjUzNjM0LCJyb2xlIjoic2VydmljZV9yb2xlIn0.necIJaiP7X2T2QjGeV-FhpkizcNTX8HjDDBAxpgQTEI"' \
-  --define:DATABASE_URL="'http://post-rest:3000'" \
-  --define:LOGTAIL_TOKEN="'secret'" \
-  --define:MAGIC_SECRET_KEY="'secret'" \
-  --define:PRIVATE_KEY='"xmbtWjE9eYuAxae9G65lQSkw36HV6H+0LSFq2aKqVwY="' \
-  --define:SALT='"secret"' \
-  --define:SENTRY_DSN='"https://000000@0000000.ingest.sentry.io/00000"' \
-  --define:MAILCHIMP_API_KEY='"secret"' \
-  --define:CLUSTER_API_URL='"http://ipfs:9094"' \
-  --define:CLUSTER_BASIC_AUTH_TOKEN='""' \
-  --define:DEBUG='false' \
-  --define:VERSION='"test"' \
-  --define:COMMITHASH='"test"' \
-  --define:BRANCH='"test"' \
+  --define:PRIVATE_KEY="'xmbtWjE9eYuAxae9G65lQSkw36HV6H+0LSFq2aKqVwY='" \
+  --define:DATABASE_CONNECTION="'$DATABASE_CONNECTION'" \
+  --define:DATABASE_TOKEN="'$DATABASE_TOKEN'" \
+  --define:DATABASE_URL="'$DATABASE_URL'" \
+  --define:LOGTAIL_TOKEN="'$LOGTAIL_TOKEN'" \
+  --define:MAGIC_SECRET_KEY="'$MAGIC_SECRET_KEY'" \
+  --define:SALT="'$SALT'" \
+  --define:SENTRY_DSN="'$SENTRY_DSN'" \
+  --define:MAILCHIMP_API_KEY="'$MAILCHIMP_API_KEY'" \
+  --define:CLUSTER_API_URL="'$CLUSTER_API_URL'" \
+  --define:CLUSTER_BASIC_AUTH_TOKEN="''" \
+  --define:DEBUG='true' \
+  --define:VERSION="'development'" \
+  --define:COMMITHASH="'development'" \
+  --define:BRANCH="'development'" \
   ./src/index.js > dist/worker.js
