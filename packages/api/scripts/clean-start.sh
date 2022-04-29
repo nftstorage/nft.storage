ce438c74045682e504021474c1dcd8f54f118afa
redaphid <iam@aaronherres.com>
2022-04-28 20:01:59 -0700

    make variables for compose file, env file locations etc. because they're moving around a lot

--- packages/api/scripts/clean-start.sh
+++ packages/api/scripts/clean-start.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env sh
-
 # This script force builds docker images, src the env, kills the old containers, and starts the new containers.
-cat ../../../.env && \
-docker-compose down --remove-orphans && \
-docker-compose build && \
-# docker-compose run --env-file="../../../.env" --service-ports api
-docker-compose --env-file="../../../.env" config && \
-docker-compose --env-file="../../../.env" up
+
+# We're moving this script around a lot, and it's pretty cwd-dependent.
+ENV_FILE=../../.env
+COMPOSE_FILE=./docker/docker-compose.yml
+
+cat $ENV_FILE && \
+docker-compose -f $COMPOSE_FILE down --remove-orphans && \
+docker-compose -f $COMPOSE_FILE build && \
+docker-compose -f $COMPOSE_FILE --env-file="$ENV_FILE" config && \
+docker-compose -f $COMPOSE_FILE --env-file="$ENV_FILE" up
