8c61b67efdcc83d7497f58773ab7394ab0abdd92
redaphid <iam@aaronherres.com>
2022-04-28 20:36:43 -0700

    start docker script.

---
+++ packages/api/scripts/start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# This script force builds docker images, src the env, kills the old containers, and starts the new containers.
+
+# We're moving this script around a lot, and it's pretty cwd-dependent.
+ENV_FILE=../../.env
+COMPOSE_FILE=./docker/docker-compose.yml
+
+cat $ENV_FILE && \
+docker-compose -f "$COMPOSE_FILE" down --remove-orphans && \
+docker-compose -f "$COMPOSE_FILE" build && \
+docker-compose -f "$COMPOSE_FILE" --env-file="$ENV_FILE" config && \
+docker-compose -f "$COMPOSE_FILE" --env-file="$ENV_FILE" up
