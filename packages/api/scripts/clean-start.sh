07af2a99bc189cadee3a4558ebcdadcf7b1283ff
redaphid <iam@aaronherres.com>
2022-04-28 19:51:21 -0700

    reorganizing pre-PR. adding some basic comments

--- packages/api/scripts/clean-start.sh
+++ packages/api/scripts/clean-start.sh
@@ -1,7 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+
+# This script force builds docker images, src the env, kills the old containers, and starts the new containers.
 cat ../../../.env && \
 docker-compose down --remove-orphans && \
 docker-compose build && \
 # docker-compose run --env-file="../../../.env" --service-ports api
 docker-compose --env-file="../../../.env" config && \
-docker-compose --env-file="../../../.env" up 
+docker-compose --env-file="../../../.env" up
--- packages/api/scripts/token-count.sh
+++ packages/api/scripts/token-count.sh
@@ -1,3 +1,5 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+
+# This counts tokens returned from the server. Useful when checking to see if the database data persists.
 API_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDY1MDA3QTczOWFiN0FDNWM1MzcxNjEyNDliODEyNTBFNDllMjg1M0MiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYzOTc1NDczNjYzOCwibmFtZSI6Im1haW4ifQ.wKwJIRXXHsgwVp8mOQp6r3_F4Lz5lnoAkgVP8wqwA_Y
 curl -vvvv 'localhost:8787/internal/tokens' -H "Authorization: Bearer $API_KEY" -H 'content-type: application/json' | jq '.value | length'
