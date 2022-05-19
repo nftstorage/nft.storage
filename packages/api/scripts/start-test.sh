fd5a0a56bbf717838de2a11beb29126f638feb82
redaphid <iam@aaronherres.com>
2022-05-18 20:10:02 -0700

    use the test profile

--- packages/api/scripts/start-test.sh
+++ packages/api/scripts/start-test.sh
@@ -3,10 +3,10 @@
 
 # We're moving this script around a lot, and it's pretty cwd-dependent.
 ENV_FILE=../../.env
-docker compose --file ./docker/docker-compose.yml --env-file="$ENV_FILE" up \
+docker compose --file ./docker/docker-compose.yml --env-file="$ENV_FILE" --profile="test" up \
   --always-recreate-deps \
   --force-recreate \
   --remove-orphans \
-  --renew-anon-volumes \
+  --renew-anon-volumes
   --build \
 ;
