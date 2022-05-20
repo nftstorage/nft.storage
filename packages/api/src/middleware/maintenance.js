8ddfeb3028135ef0052052f1ed50f552e2ea1684
Yusef Napora <yusef@protocol.ai>
2022-05-20 15:29:03 -0400

    fixup for prev commit: get maintenance mode from config

--- packages/api/src/middleware/maintenance.js
+++ packages/api/src/middleware/maintenance.js
@@ -1,4 +1,5 @@
 import { ErrorMaintenance, HTTPError } from '../errors.js'
+import { getServiceConfig } from '../config.js'
 
 /**
  * @typedef {'rw' | 'r-' | '--'} Mode
@@ -29,26 +30,7 @@ export const modes = Object.freeze([NO_READ_OR_WRITE, READ_ONLY, READ_WRITE])
 export const DEFAULT_MODE = READ_WRITE
 
 /** @type {() => Mode} */
-let getMaintenanceMode = () => DEFAULT_MODE
-
-/**
- * Sets the function that returns the current maintenance mode. This allows us
- * to pass a function that accesses a global variable. Cloudflare secrets are
- * exposed as global variables so this is the way to dynamically access them.
- *
- * The return value from the getter function dictates which request handlers
- * (that have been wrapped with `withMode`) are enabled. The returned value is
- * one of:
- *
- * -- = no reading or writing
- * r- = read only mode
- * rw = read and write (normal operation)
- *
- * @param {() => Mode} getter
- */
-export function setMaintenanceModeGetter(getter) {
-  getMaintenanceMode = getter
-}
+const getMaintenanceMode = () => getServiceConfig().MAINTENANCE_MODE
 
 /**
  * Specify the mode (permissions) a request hander requires to operate e.g.
