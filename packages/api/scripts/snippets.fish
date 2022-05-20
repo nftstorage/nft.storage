ff96394d3c10373cce433c65179e18d31ed39497
redaphid <iam@aaronherres.com>
2022-05-20 12:27:58 -0700

    remove snippets.fish

--- packages/api/scripts/snippets.fish
+++
@@ -1,6 +0,0 @@
-# Snippet to run playwright tests via docker-compose run:
-set compose_args "-f ./docker/docker-compose.yml -f ./docker/docker-compose.test.yml"
-docker-compose $compose_args down --remove-orphans
-and docker-compose $compose_args build
-and docker-compose $compose_args run playwright
-ls
