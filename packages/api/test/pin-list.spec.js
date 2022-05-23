65eda5a38ac62fe67ba60b718fcc9b70c5a36146
Alan Shaw <alan.shaw@protocol.ai>
2022-05-23 16:20:50 +0100

    fix: default PSA pin status filter

--- packages/api/test/pin-list.spec.js
+++ packages/api/test/pin-list.spec.js
@@ -5,7 +5,7 @@ describe('Pin list ', () => {
   /** @type{DBTestClient} */
   let client
 
-  before(async () => {
+  beforeEach(async () => {
     client = await createClientWithUser()
   })
 
@@ -68,6 +68,50 @@ describe('Pin list ', () => {
       valuePinList.results[0].requestid,
       cid,
       'Server response with pin requests created'
+    )
+  })
+
+  it('should list pinned items when querying without filters', async () => {
+    // List
+    const resEmptyPinList = await fetch('pins', {
+      method: 'GET',
+      headers: { Authorization: `Bearer ${client.token}` },
+    })
+    const valueEmptyPinList = await resEmptyPinList.json()
+    assert.strictEqual(
+      valueEmptyPinList.count,
+      0,
+      'Server response with empty pin requests'
+    )
+
+    // Pin request
+    const cid = 'bafkreihwlixzeusjrd5avlg53yidaoonf5r5srzumu7y5uuumtt7rxxbrm'
+    const resPinCreate = await fetch('pins', {
+      method: 'POST',
+      headers: { Authorization: `Bearer ${client.token}` },
+      body: JSON.stringify({ cid }),
+    })
+    assert.ok(resPinCreate.ok, 'Server responde ok')
+
+    // Upload
+    const file = new Blob(['hello world!'], { type: 'application/text' })
+    const res = await fetch('upload', {
+      method: 'POST',
+      headers: { Authorization: `Bearer ${client.token}` },
+      body: file,
+    })
+    assert(res.ok, 'Server response ok')
+
+    // List
+    const resPinList = await fetch('pins', {
+      method: 'GET',
+      headers: { Authorization: `Bearer ${client.token}` },
+    })
+    const valuePinList = await resPinList.json()
+    assert.strictEqual(
+      valuePinList.count,
+      1,
+      'Server response with 1 successful pin request'
     )
   })
 })
