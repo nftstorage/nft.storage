6a6b217837e95750cfd5ad4d6e91b5f2badfa404
redaphid <iam@aaronherres.com>
2022-05-24 23:36:27 -0700

    fix omission of ipfs-cluster for api dependencies; use 'docker compose run' to get the exit code for now

--- packages/api/scripts/start-test.sh
+++ packages/api/scripts/start-test.sh
@@ -3,13 +3,11 @@
 
 # We're moving this script around a lot, and it's pretty cwd-dependent.
 ENV_FILE=../../.env
-docker compose --profile="test" --file ./docker/docker-compose.yml --env-file="$ENV_FILE" config
+docker compose --project-name="nft-storage-api-test" --profile="test" --file ./docker/docker-compose.yml --env-file="$ENV_FILE" build
+docker compose --project-name="nft-storage-api-test" --profile="test" --file ./docker/docker-compose.yml --env-file="$ENV_FILE" down
 
-docker compose \
-  --project-name="nft-storage-api-test" \
-  --profile="test" --file ./docker/docker-compose.yml --env-file="$ENV_FILE" up \
-  --always-recreate-deps \
-  --force-recreate \
-  --renew-anon-volumes \
-  --build \
-;
+docker compose --project-name="nft-storage-api-test" --profile="test" --file ./docker/docker-compose.yml --env-file="$ENV_FILE" run --rm playwright
+test_exit_code=$?
+
+docker compose --project-name="nft-storage-api-test" --profile="test" --file ./docker/docker-compose.yml --env-file="$ENV_FILE" down
+exit "$test_exit_code"
