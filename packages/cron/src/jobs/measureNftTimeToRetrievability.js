82f97a5234cab325a99248104e56ebf1e3f0e078
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2022-05-25 15:54:26 -0700

    nft-ttr downloads a source file to use for rest of test

--- packages/cron/src/jobs/measureNftTimeToRetrievability.js
+++ packages/cron/src/jobs/measureNftTimeToRetrievability.js
@@ -8,30 +8,45 @@ import fetch from '@web-std/fetch'
 const FILENAME_TO_UPLOAD = 'to-upload'
 
 /**
- * @param {object} config
- * @param {string} [config.url] - URL to nft.storage to measure
+ * Download an image to the directory and return path to the file
+ * @param {string} targetDir - path to fs dir to download to
  */
-export async function measureNftTimeToRetrievability({
-  url = 'https://nft.storage',
-} = {}) {
-  console.log('start measureNftTimeToRetrievability', { url })
-  const dir = await createRandomDir()
+const downloadRandomImageToUpload = async (targetDir) => {
   const { target: file } = await downloadFile(
     new URL(
       'https://bafybeiarmhq3d7msony7zfq67gmn46syuv6jrc6dagob2wflunxiyaksj4.ipfs.dweb.link/1681.png'
     ),
-    ({ ext }) => path.join(dir, `${FILENAME_TO_UPLOAD}.${ext}`)
+    ({ ext }) => path.join(targetDir, `${FILENAME_TO_UPLOAD}.${ext}`)
   )
+  return file
+}
+
+/**
+ * Job that tests/measures steps
+ * * prepare a sample image
+ * * upload to nft.storage
+ * * retrieve image through ipfs gateway
+ * @param {object} config
+ * @param {string} [config.url] - URL to nft.storage to measure
+ */
+export async function measureNftTimeToRetrievability(config = {}) {
+  console.log('start measureNftTimeToRetrievability', config)
+  const dir = await createRandomDir()
+  const file = await downloadRandomImageToUpload(dir)
   console.log({ dir, upload: { source: file } })
 }
 
+/**
+ * Create a new dir on the fs that can be used to test in
+ * @returns fs path to dir
+ */
 async function createRandomDir() {
   const randomDir = await mkdtemp(`${tmpdir()}${path.sep}nft-ttr-`)
   return randomDir
 }
 
 /**
- *
+ * Download a file from a source url to the filesystem
  * @param {URL} source
  * @param {(o: { ext: string }) => string} createTargetPath
  */
