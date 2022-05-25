c47119a9d1577553682f52d04b23a5ba2fa3240b
redaphid <iam@aaronherres.com>
2022-05-25 15:19:08 -0700

    try the previous syntax for playwright testing and see if it works in Github Actions now

--- packages/api/scripts/start-test.sh
+++ packages/api/scripts/start-test.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env sh
-# This script force builds docker images, src the env, kills the old containers, and starts the new containers.
 
-# We're moving this script around a lot, and it's pretty cwd-dependent.
 ENV_FILE=../../.env
-DOCKER_COMMAND="docker compose --project-name=nft-storage-api-test --profile=test --file ./docker/docker-compose.yml --env-file=$ENV_FILE"
-$DOCKER_COMMAND build
-$DOCKER_COMMAND down
-$DOCKER_COMMAND run --rm  playwright
-test_exit_code=$?
+docker compose --profile="test" --file ./docker/docker-compose.yml --env-file="$ENV_FILE" config
 
-$DOCKER_COMMAND down
-exit "$test_exit_code"
+docker compose \
+  --project-name="nft-storage-api-test" \
+  --profile="test" --file ./docker/docker-compose.yml --env-file="$ENV_FILE" up \
+  --always-recreate-deps \
+  --force-recreate \
+  --renew-anon-volumes \
+  --build \
+  --attach "playwright" \
+  ;
+
