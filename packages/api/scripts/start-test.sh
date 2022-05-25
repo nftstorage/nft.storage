d2ae2e2dfb1f611b154ddd6f3da03c3e15c657e1
redaphid <iam@aaronherres.com>
2022-05-24 23:42:19 -0700

    DRY up the start-test.sh script

--- packages/api/scripts/start-test.sh
+++ packages/api/scripts/start-test.sh
@@ -3,11 +3,11 @@
 
 # We're moving this script around a lot, and it's pretty cwd-dependent.
 ENV_FILE=../../.env
-docker compose --project-name="nft-storage-api-test" --profile="test" --file ./docker/docker-compose.yml --env-file="$ENV_FILE" build
-docker compose --project-name="nft-storage-api-test" --profile="test" --file ./docker/docker-compose.yml --env-file="$ENV_FILE" down
-
-docker compose --project-name="nft-storage-api-test" --profile="test" --file ./docker/docker-compose.yml --env-file="$ENV_FILE" run --rm playwright
+DOCKER_COMMAND="docker compose --project-name=nft-storage-api-test --profile=test --file ./docker/docker-compose.yml --env-file=$ENV_FILE"
+$DOCKER_COMMAND build
+$DOCKER_COMMAND down
+$DOCKER_COMMAND run --rm playwright
 test_exit_code=$?
 
-docker compose --project-name="nft-storage-api-test" --profile="test" --file ./docker/docker-compose.yml --env-file="$ENV_FILE" down
+$DOCKER_COMMAND down
 exit "$test_exit_code"
