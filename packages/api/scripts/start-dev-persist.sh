bfee0c3597e29b6d9d843a3ccbc8a5b5e029a760
redaphid <iam@aaronherres.com>
2022-05-31 16:25:31 -0700

    make project name for the persistent dev environment different than the immutable one

--- packages/api/scripts/start-dev-persist.sh
+++ packages/api/scripts/start-dev-persist.sh
@@ -5,7 +5,7 @@
 ENV_FILE=../../.env
 COMPOSE_FILES="--file ./docker/docker-compose.yml --file ./docker/docker-compose.dev.yml --file ./docker/docker-compose.persist.yml"
 docker compose \
-  --project-name="nft-storage-api" \
+  --project-name="nft-storage-api-persist" \
   $COMPOSE_FILES --env-file="$ENV_FILE" up \
   --build \
   --remove-orphans \
