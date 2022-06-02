3d272d86152ab2ee3b3b10fa9ccac5049fcd91f2
redaphid <iam@aaronherres.com>
2022-06-02 16:35:29 -0700

    make a healthcheck for the database, so  we don't rely on `db-init`'s exit code, as this was causing problems in the 'persist' mode. Added a `yarn dev:clean` to clean the volumes for the persist mode. Managed to remove one docker-compose file :)

--- packages/api/scripts/clean-dev-persist.sh
+++ packages/api/scripts/clean-dev-persist.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env sh
-# This script force builds docker images, src the env, kills the old containers, and starts the new containers.
-
-# We're moving this script around a lot, and it's pretty cwd-dependent
-docker compose --project-name="nft-storage-api-persist" rm --volumes
+docker compose --project-name="nft-storage-api-persist" rm --force --volumes
--- packages/api/scripts/start-dev-persist.sh
+++ packages/api/scripts/start-dev-persist.sh
@@ -3,7 +3,7 @@
 
 # We're moving this script around a lot, and it's pretty cwd-dependent.
 ENV_FILE=../../.env
-COMPOSE_FILES="--file ./docker/docker-compose.yml --file ./docker/docker-compose.dev.yml --file ./docker/docker-compose.persist.yml"
+COMPOSE_FILES="--file ./docker/docker-compose.yml --file ./docker/docker-compose.dev.yml"
 docker compose \
   --project-name="nft-storage-api-persist" \
   $COMPOSE_FILES --env-file="$ENV_FILE" up \
