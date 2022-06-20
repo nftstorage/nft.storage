8d66a7159dced78d726ca1540829ab3d1b11d649
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2022-06-20 14:45:11 -0400

    chore(cron): use type assertion instead of runtime assert in src/lib/pinata.js

--- packages/cron/src/lib/pinata.js
+++ packages/cron/src/lib/pinata.js
@@ -1,8 +1,6 @@
 import { URL } from 'url'
 import { RateLimiter } from 'limiter'
 import { fetchJSON } from './fetch.js'
-import assert from 'assert'
-import { hasOwnProperty } from './utils.js'
 
 const endpoint = 'https://api.pinata.cloud'
 
@@ -30,29 +28,17 @@ export class Pinata {
    */
   async pinByHash(cid, options) {
     const url = new URL('/pinning/pinByHash', endpoint)
-    const json = await fetchJSON(this.limiter, url.toString(), {
-      method: 'POST',
-      headers: {
-        authorization: `Bearer ${this.apiToken}`,
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify({ hashToPin: cid, ...(options || {}) }),
-    })
-    assert.ok(typeof json === 'object')
-    assert.ok(json)
-    assert.ok(hasOwnProperty(json, 'id'))
-    const id = json.id
-    assert.ok(typeof id === 'string')
-    assert.ok(hasOwnProperty(json, 'ipfsHash'))
-    const ipfsHash = json.ipfsHash
-    assert.ok(typeof ipfsHash === 'string')
-    assert.ok(hasOwnProperty(json, 'status'))
-    const status = json.status
-    assert.ok(typeof status === 'string')
-    assert.ok(hasOwnProperty(json, 'name'))
-    const name = json.name
-    assert.ok(typeof name === 'string')
-    const pin = { id, ipfsHash, status, name }
+    const json = /** @type {unknown} */ (
+      await fetchJSON(this.limiter, url.toString(), {
+        method: 'POST',
+        headers: {
+          authorization: `Bearer ${this.apiToken}`,
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({ hashToPin: cid, ...(options || {}) }),
+      })
+    )
+    const pin = /** @type {PinataPin} */ (json)
     return pin
   }
 }
