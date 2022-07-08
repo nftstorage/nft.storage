23abfeb070a53bd8199950412046a27462c8c5ff
Yusef Napora <yusef@napora.org>
2022-07-08 13:27:20 +0000

    ava-fy nfts-get and nfts-list tests

--- packages/api/test/nfts-get.spec.js
+++ packages/api/test/nfts-get.spec.js
@@ -1,101 +1,109 @@
-import assert from 'assert'
-import { createClientWithUser, DBTestClient } from './scripts/helpers.js'
+import test from 'ava'
+import { createClientWithUser } from './scripts/helpers.js'
 import { fixtures } from './scripts/fixtures.js'
+import {
+  getMiniflareContext,
+  setupMiniflareContext,
+} from './scripts/test-context.js'
 
-describe('Get NFT', () => {
-  /** @type{DBTestClient} */
-  let client
+test.beforeEach(async (t) => {
+  await setupMiniflareContext(t)
+})
 
-  before(async () => {
-    client = await createClientWithUser()
+test('should return proper response for cid v1', async (t) => {
+  const cid = 'bafybeiaj5yqocsg5cxsuhtvclnh4ulmrgsmnfbhbrfxrc3u2kkh35mts4e'
+  const client = await createClientWithUser(t)
+  const mf = getMiniflareContext(t)
+  await client.addPin({
+    cid,
+    name: 'test-file11',
   })
 
-  it('should return proper response for cid v1', async () => {
-    const cid = 'bafybeiaj5yqocsg5cxsuhtvclnh4ulmrgsmnfbhbrfxrc3u2kkh35mts4e'
-    await client.addPin({
-      cid,
-      name: 'test-file11',
-    })
+  const res = await mf.dispatchFetch(`http://localhost:8787/${cid}`, {
+    headers: { Authorization: `Bearer ${client.token}` },
+  })
+  const { ok, value } = await res.json()
 
-    const res = await fetch(cid, {
-      headers: { Authorization: `Bearer ${client.token}` },
-    })
-    const { ok, value } = await res.json()
+  t.is(value.cid, cid)
+  t.is(value.type, 'remote')
+  t.is(value.pin.status, 'queued')
+  t.deepEqual(value.deals, fixtures.dealsV0andV1)
+})
 
-    assert.equal(value.cid, cid)
-    assert.equal(value.type, 'remote')
-    assert.equal(value.pin.status, 'queued')
-    assert.deepStrictEqual(value.deals, fixtures.dealsV0andV1)
+test('should return proper response for cid v0', async (t) => {
+  const cid = 'QmP1QyqiRtQLbGBr5hLVX7NCmrJmJbGdp45x6DnPssMB9i'
+  const client = await createClientWithUser(t)
+  const mf = getMiniflareContext(t)
+  await client.addPin({
+    cid,
+    name: 'test-file-cid-v0',
   })
 
-  it('should return proper response for cid v0', async () => {
-    const cid = 'QmP1QyqiRtQLbGBr5hLVX7NCmrJmJbGdp45x6DnPssMB9i'
-    await client.addPin({
-      cid,
-      name: 'test-file-cid-v0',
-    })
+  const res = await mf.dispatchFetch(`http://localhost:8787/${cid}`, {
+    headers: { Authorization: `Bearer ${client.token}` },
+  })
+  const { ok, value } = await res.json()
+  t.is(value.cid, cid)
+  t.is(value.pin.status, 'queued')
+  t.deepEqual(value.deals, fixtures.dealsV0andV1)
+})
 
-    const res = await fetch(cid, {
-      headers: { Authorization: `Bearer ${client.token}` },
-    })
-    const { ok, value } = await res.json()
-    assert.equal(value.cid, cid)
-    assert.equal(value.pin.status, 'queued')
-    assert.deepStrictEqual(value.deals, fixtures.dealsV0andV1)
+test('should error on invalid cid', async (t) => {
+  const cid = 'asdhjkahsdja'
+  const client = await createClientWithUser(t)
+  const mf = getMiniflareContext(t)
+  const res = await mf.dispatchFetch(`http://localhost:8787/${cid}`, {
+    headers: { Authorization: `Bearer ${client.token}` },
+  })
+  const { ok, value, error } = await res.json()
+
+  t.false(ok)
+  t.deepEqual(error, {
+    code: 'ERROR_INVALID_CID',
+    message: `Invalid CID: ${cid}`,
+  })
+})
+
+test('should error on not found', async (t) => {
+  const cid = 'bafybeia22kh3smc7p67oa76pcleaxp4u5zatsvcndi3xrqod5vtxq5avpa'
+  const client = await createClientWithUser(t)
+  const mf = getMiniflareContext(t)
+  const res = await mf.dispatchFetch(`http://localhost:8787/${cid}`, {
+    headers: { Authorization: `Bearer ${client.token}` },
+  })
+  const { ok, value, error } = await res.json()
+
+  t.false(ok)
+  t.deepEqual(error, {
+    code: 'HTTP_ERROR',
+    message: `NFT not found`,
+  })
+})
+
+test('should error on not found for a deleted nft', async (t) => {
+  const client = await createClientWithUser(t)
+  const mf = getMiniflareContext(t)
+  const cidv1 = 'bafybeiaj5yqocsg5cxsuhtvclnh4ulmrgsmnfbhbrfxrc3u2kkh35mts4e'
+  await client.client.createUpload({
+    content_cid: cidv1,
+    source_cid: cidv1,
+    type: 'Blob',
+    user_id: client.userId,
+    dag_size: 100,
   })
 
-  it('should error on invalid cid', async () => {
-    const cid = 'asdhjkahsdja'
-    const res = await fetch(cid, {
-      headers: { Authorization: `Bearer ${client.token}` },
-    })
-    const { ok, value, error } = await res.json()
+  const deleted = await client.client.deleteUpload(cidv1, client.userId)
+  t.truthy(deleted)
+  t.is(deleted && deleted.source_cid, cidv1)
 
-    assert.equal(ok, false)
-    assert.deepStrictEqual(error, {
-      code: 'ERROR_INVALID_CID',
-      message: `Invalid CID: ${cid}`,
-    })
+  const res = await mf.dispatchFetch(`http://localhost:8787/${cidv1}`, {
+    headers: { Authorization: `Bearer ${client.token}` },
   })
+  const { ok, error } = await res.json()
 
-  it('should error on not found', async () => {
-    const cid = 'bafybeia22kh3smc7p67oa76pcleaxp4u5zatsvcndi3xrqod5vtxq5avpa'
-    const res = await fetch(cid, {
-      headers: { Authorization: `Bearer ${client.token}` },
-    })
-    const { ok, value, error } = await res.json()
-
-    assert.equal(ok, false)
-    assert.deepStrictEqual(error, {
-      code: 'HTTP_ERROR',
-      message: `NFT not found`,
-    })
-  })
-
-  it('should error on not found for a deleted nft', async () => {
-    const client = await createClientWithUser()
-    const cidv1 = 'bafybeiaj5yqocsg5cxsuhtvclnh4ulmrgsmnfbhbrfxrc3u2kkh35mts4e'
-    await client.client.createUpload({
-      content_cid: cidv1,
-      source_cid: cidv1,
-      type: 'Blob',
-      user_id: client.userId,
-      dag_size: 100,
-    })
-
-    const deleted = await client.client.deleteUpload(cidv1, client.userId)
-    assert.ok(deleted)
-    assert.equal(deleted.source_cid, cidv1)
-
-    const res = await fetch(cidv1, {
-      headers: { Authorization: `Bearer ${client.token}` },
-    })
-    const { ok, error } = await res.json()
-
-    assert.equal(ok, false, 'not found')
-    assert.deepStrictEqual(error, {
-      code: 'HTTP_ERROR',
-      message: `NFT not found`,
-    })
+  t.false(ok, 'not found')
+  t.deepEqual(error, {
+    code: 'HTTP_ERROR',
+    message: `NFT not found`,
   })
 })
