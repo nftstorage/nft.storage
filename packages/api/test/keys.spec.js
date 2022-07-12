7bab872360c399fd7fc426a45c979f2971d0404e
Yusef Napora <yusef@napora.org>
2022-07-12 19:10:39 +0000

    de-flake test (order of returned keys can vary)

--- packages/api/test/keys.spec.js
+++ packages/api/test/keys.spec.js
@@ -205,7 +205,9 @@ test.serial("should not be able to delete another user's key", async (t) => {
   const { ok, value } = await resList.json()
 
   t.is(value.length, 2, 'should still have the default key and key1')
-  t.is(value[1].name, 'test-key-1')
+  t.true(
+    value.some((/** @type {{ name: string }} */ v) => v.name === 'test-key-1')
+  )
 })
 
 test.serial('should not delete a deleted key', async (t) => {
