c6cfd3fa9500a10b68247a9b98f7fb244f747f5e
Yusef Napora <yusef@napora.org>
2022-07-12 19:18:42 +0000

    re-enable version test (set to fixed values in test env during build)

--- packages/api/test/version.spec.js
+++ packages/api/test/version.spec.js
@@ -9,8 +9,7 @@ test.before(async (t) => {
   await setupMiniflareContext(t, { noContainers: true })
 })
 
-// FIXME: esbuild is setting the real version number in the worker, but we don't have access to it in the test scope...
-test.skip('should get version information', async (t) => {
+test('should get version information', async (t) => {
   const mf = getMiniflareContext(t)
   const config = getTestServiceConfig(t)
 
