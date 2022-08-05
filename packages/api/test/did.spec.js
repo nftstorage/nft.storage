972f89f1e5163d1214680192966026439e4c98d7
Yusef Napora <yusef@protocol.ai>
2022-08-05 13:05:57 -0400

    chore: convert tests to use miniflare + ava (#2065)
    * wip: convert tests to ava/miniflare
    
    * refactor test helpers
    
    * avoid loading config in global scope
    
    * add testcontainers and use to ava-fy db-client.spec.js
    
    * ava-fy did.spec.js
    
    * ava-fy keys.spec.js and use test-specific service config
    
    * wip: cluster in a testcontainer
    
    * more avafication
    
    * ava-fy nfts-get and nfts-list tests
    
    * add minio and afa-fy metaplex upload test
    
    * ava-fy nfts-upload tests
    
    * almost all tests passing with ava, but some docker flakiness
    
    * faster nfts-list test
    
    * use ava shared worker for test containers (much faster)
    
    * wire up ava to yarn test
    
    * add special metaplex user when creating db test schema
    
    * use node 18.4.0 in ci
    
    * only require node 18+ for api package
    
    * use node 18.4.0 in all workflows
    
    * use newer ubuntu for website workflow
    
    * wip: move test helper code to miniflare worker
    
    * remove tweetnacl, move more stuff into test worker
    
    * remove external node:crypto from esbuild config
    
    * de-flake test (order of returned keys can vary)
    
    * update test-worker README and rm deps from package.json
    
    * rm old config code
    
    * re-enable version test (set to fixed values in test env during build)
    
    * update comments & link to miniflare PRs
    
    * remove engines restriction on api package
    
    * add ava.config.js
    
    * use one-webcrypto and @noble/ed25519
    
    * de-flake test (same as previous)
    
    * rm debug log
    
    * rewrite version test to avoid special-casing the build for test env
    
    * remove miniflare test worker (not needed with node 18+)
    
    * fix(website): pin nextra dependency to avoid build error
    
    * simplify testcontainers setup by using docker-compose
    
    * don't install playwright deps in ci
    
    * pull docker dependencies before running tests
    
    * debug: print container logs during test to debug ci issue
    
    * use log message wait strategy for all containers
    
    * use explicit container_name in docker compose
    
    * rm debug stuff
    
    * fix test expectation (rebase mishap)
    
    * ci: only use node 18 for api package
    
    * rm debug logs and unneeded tsconfig change
    
    * use same .env file for dev and test
    
    * copy .env.tpl to .env in ci
    
    * use COMPOSE_COMPATIBILITY flag instead of setting fixed container_names
    
    * remove option to bind globals in test scope, rewrite tests that used it
    
    * fix typo
    
    * rebase fixup
    
    * replace testcontainers with vanilla docker compose & remove ava shared worker
    
    * rm debug log
    
    * use http://miniflare.test in dispatchFetch calls
    
    The host value is ignored when calling `mf.dispatchFetch`.
    I was using `localhost:8787`, but that's confusing, since it implies
    a service may really be listening on that port
    
    * return dev env to status quo
    
    * Update packages/api/package.json
    
    Co-authored-by: Vasco Santos <santos.vasco10@gmail.com>
    
    Co-authored-by: Vasco Santos <santos.vasco10@gmail.com>

--- packages/api/test/did.spec.js
+++ packages/api/test/did.spec.js
@@ -1,13 +1,18 @@
-import assert from 'assert'
+import test from 'ava'
+import {
+  setupMiniflareContext,
+  getMiniflareContext,
+} from './scripts/test-context.js'
 
-describe('Service DID', () => {
-  it('should return the service DID', async () => {
-    const res = await fetch(`did`)
-    const { ok, value } = await res.json()
+test.before(async (t) => {
+  await setupMiniflareContext(t)
+})
 
-    assert.equal(
-      value,
-      'did:key:z6MkgBRFJi7Ew2mzN7JtYN7nkq8M8qTHhLbsTEvV5j4ajWZS'
-    )
-  })
+test('/did should return the service DID', async (t) => {
+  const mf = getMiniflareContext(t)
+  const res = await mf.dispatchFetch(`http://miniflare.test/did`)
+  const { ok, value } = await res.json()
+
+  t.true(ok)
+  t.is(value, 'did:key:z6MkgBRFJi7Ew2mzN7JtYN7nkq8M8qTHhLbsTEvV5j4ajWZS')
 })
