23f54cd80fb62dabb184dd4e8c66af572bbd1e75
Ericsson Oliveira <cssonddos@gmail.com>
2022-08-18 11:06:45 -0300

    feat: add logging for mailchimp

--- packages/api/src/routes/blog-subscribe.js
+++ packages/api/src/routes/blog-subscribe.js
@@ -5,6 +5,7 @@ const SERVER_PREFIX = 'us5'
 const LIST_ID = '64f6e3fd11'
 const API_KEY = getServiceConfig().MAILCHIMP_API_KEY
 const TOKEN = btoa(`:${API_KEY}`)
+const urlPrefix = `https://${SERVER_PREFIX}.api.mailchimp.com/3.0/lists/${LIST_ID}/members/`
 
 const headers = {
   'Content-Type': 'application/json; charset=UTF-8',
@@ -15,13 +16,23 @@ const headers = {
  *  @param {string} email
  */
 export const isChimpUser = async (email) => {
-  const url = `https://${SERVER_PREFIX}.api.mailchimp.com/3.0/lists/${LIST_ID}/members/${encodeURIComponent(
-    email
-  )}`
+  const url = `${urlPrefix}${encodeURIComponent(email)}`
+
+  var t = Date.now()
   const res = await fetch(url, {
     method: 'GET',
     headers,
   })
+  try {
+    console.log(
+      JSON.stringify({
+        timeTaken: Date.now() - t,
+        method: 'GET',
+        url: urlPrefix + 'email',
+        status: res.status,
+      })
+    )
+  } catch (e) {}
   return res.ok
 }
 
@@ -29,36 +40,59 @@ export const isChimpUser = async (email) => {
  *  @param {string} email
  */
 export const addSubscriber = async (email) => {
-  const url = `https://${SERVER_PREFIX}.api.mailchimp.com/3.0/lists/${LIST_ID}/members/`
+  const url = urlPrefix
   const body = JSON.stringify({
     email_address: email,
     status: 'subscribed',
     tags: ['nft_storage_blog_subscriber'],
   })
-
-  return await fetch(url, {
+  var t = Date.now()
+  const res = await fetch(url, {
     method: 'POST',
     headers,
     body,
   })
+  try {
+    console.log(
+      JSON.stringify({
+        timeTaken: Date.now() - t,
+        mailChimp: false,
+        method: 'POST',
+        url: url,
+        status: res.status,
+      })
+    )
+  } catch (e) {}
+  return res
 }
 
 /**
  *  @param {string} email
  */
 const updateSubscriber = async (email) => {
-  const url = `https://${SERVER_PREFIX}.api.mailchimp.com/3.0/lists/${LIST_ID}/members/${encodeURIComponent(
-    email
-  )}/tags`
+  const url = `${urlPrefix}${encodeURIComponent(email)}/tags`
+
   const body = JSON.stringify({
     tags: [{ name: 'nft_storage_blog_subscriber', status: 'active' }],
   })
-
-  return await fetch(url, {
+  var t = Date.now()
+  const res = await fetch(url, {
     method: 'POST',
     headers,
     body,
   })
+  try {
+    console.log(
+      JSON.stringify({
+        timeTaken: Date.now() - t,
+        mailChimp: true,
+        method: 'POST',
+        url: urlPrefix + 'email/tags',
+        status: res.status,
+      })
+    )
+  } catch (e) {}
+  return res
 }
 
 /** @type {import('../bindings').Handler} */
