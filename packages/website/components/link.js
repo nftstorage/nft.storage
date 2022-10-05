2465ec6ef362c3f0aba1e3736d889deff136caac
e-schneid <99349687+e-schneid@users.noreply.github.com>
2022-10-05 08:40:46 -0700

    fix: open blog link in new window (#2211)

--- packages/website/components/link.js
+++ packages/website/components/link.js
@@ -7,15 +7,23 @@ import Link from 'next/link'
  * @param {string} [props.className]
  * @param {number} [props.tabIndex]
  * @param {string} [props.id]
+ * @param {string} [props.target]
  * @param {React.MouseEventHandler} [props.onClick]
  * @param {React.ReactNode | string} props.children
  * @returns {JSX.Element}
  */
-const WrappedLink = ({ tabIndex = 0, href = '', children, ...otherProps }) => (
-  <Link href={href} {...otherProps}>
+const WrappedLink = ({
+  tabIndex = 0,
+  href = '',
+  children,
+  target,
+  ...otherProps
+}) => (
+  <Link href={href} {...otherProps} passHref={target === '_blank'}>
     <a
       href={href.pathname || href}
       {...otherProps}
+      target={target}
       tabIndex={tabIndex}
       onClick={otherProps.onClick}
     >
