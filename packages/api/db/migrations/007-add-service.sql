102b303e477d890408eb361c0a5583cb8fd5242c
Oli Evans <oli@protocol.ai>
2022-10-12 15:29:21 +0100

    chore: fix db migration
    License: MIT
    Signed-off-by: Oli Evans <oli@protocol.ai>

--- packages/api/db/migrations/007-add-service.sql
+++ packages/api/db/migrations/007-add-service.sql
@@ -1,13 +1,1 @@
--- Service type is the place/location/organisation that is pinning the content.
-CREATE OR UPDATE TYPE service_type AS ENUM (
-    -- The NFT.Storage cluster in Pinata (LEGACY).
-    'Pinata',
-    -- The original NFT.Storage cluster.
-    'IpfsCluster',
-    -- An IPFS Cluster originally commissioned for niftysave.
-    'IpfsCluster2',
-    -- New cluster with flatfs and better DHT.
-    'IpfsCluster3'
-    -- The big one.
-    'ElasticIpfs'
-    );
+ALTER TYPE service_type ADD VALUE 'ElasticIpfs';
