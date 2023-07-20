2bd182fe02ccdb3d512546d59c485ee504705e30
Irakli Gozalishvili <contact@gozala.io>
2023-07-20 08:30:31 -0700

    chore: delete solona guide (#2423)
    Removes solana guide per
    https://filecoinproject.slack.com/archives/C02BZPRS9HP/p1689628803771479

--- packages/website/next.config.js
+++ packages/website/next.config.js
@@ -62,7 +62,6 @@ const nextConfig = withBundleAnalyzer({
       '/docs/how-to/pinning-service': {
         page: '/docs/how-to/pinning-service',
       },
-      '/docs/how-to/mint-solana': { page: '/docs/how-to/mint-solana' },
       '/docs/how-to/retrieve': { page: '/docs/how-to/retrieve' },
       '/docs/how-to/store-directory': { page: '/docs/how-to/store-directory' },
       '/docs/how-to/get-status': { page: '/docs/how-to/get-status' },
