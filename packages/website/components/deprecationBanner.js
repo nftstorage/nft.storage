581e33e10f2e319c14c7e6df2d479bd4c36b8894
Travis Vachon <travis.vachon@gmail.com>
2024-04-04 13:02:10 -0700

    fix: adjust banner
    point to the blogpost in all cases because of #11 from https://github.com/orgs/w3s-project/projects/1/views/1?pane=issue&itemId=57594940

--- packages/website/components/deprecationBanner.js
+++ packages/website/components/deprecationBanner.js
@@ -2,7 +2,9 @@ export function DeprecationBanner() {
   return (
     <div className="w-full text-center p-3 bg-black text-white text-sm">
       We're evolving our platform for you. Learn more &gt;&gt;&nbsp;
-      <a href="https://nft.storage">https://nft.storage</a>
+      <a href="https://nft.storage/blog/the-next-chapter-of-nftstorage">
+        https://nft.storage/blog/the-next-chapter-of-nftstorage
+      </a>
     </div>
   )
 }
