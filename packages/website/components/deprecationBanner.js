2235de685231437eb5c6bd595b403be521d80173
Travis Vachon <travis.vachon@gmail.com>
2024-04-04 12:03:32 -0700

    feat: add deprecation banner
    #3 from https://github.com/orgs/w3s-project/projects/1/views/1?pane=issue&itemId=57594940

---
+++ packages/website/components/deprecationBanner.js
@@ -0,0 +1,8 @@
+export function DeprecationBanner() {
+  return (
+    <div className="w-full text-center p-3 bg-black text-white text-sm">
+      We're evolving our platform for you. Learn more &gt;&gt;&nbsp;
+      <a href="https://nft.storage">https://nft.storage</a>
+    </div>
+  )
+}
